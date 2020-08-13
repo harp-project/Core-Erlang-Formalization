957533ec3fb9b41dc930a5f8f6fe512e67cc17fc
berpeti <peti.ber007@gmail.com>
2020-08-13 13:15:55 +0200

    Increase the speed of the evaluation tactic

--- src/Core_Erlang_Tactics.v
+++ src/Core_Erlang_Tactics.v
@@ -23,6 +23,148 @@ Qed.
 
 End Helper_Theorems.
 
+Fixpoint is_literal_list (l : list Expression) : bool :=
+match l with
+| [] => true
+| (ELit _)::xs => is_literal_list xs
+| _ => false
+end.
+
+Fixpoint literal_list_to_value_list (l : list Expression) : list Value :=
+match l with
+| [] => []
+| (ELit x)::xs => (VLit x)::(literal_list_to_value_list xs)
+| _ => []
+end.
+
+Theorem eval_lit_list :
+  forall l env id eff,
+  is_literal_list l = true
+->
+  (forall i, i < length l -> |env, id, nth i l ErrorExp, eff| 
+  -e> 
+  |id, inl (nth i (literal_list_to_value_list l) ErrorValue), eff|).
+Proof.
+  induction l; intros.
+  * inversion H0.
+  * simpl in H0, H. destruct i.
+    - simpl. destruct a; try (inversion H).
+      + simpl. apply eval_lit.
+    - simpl. destruct a; try (inversion H). simpl. apply IHl.
+      + assumption.
+      + omega.
+Qed.
+
+Fixpoint make_list {A : Type} (e : A) (n : nat) : list A :=
+match n with
+| 0 => []
+| S n' => e::(make_list e n')
+end.
+
+Theorem length_lit_list :
+  forall l, is_literal_list l = true -> 
+  length l = length (literal_list_to_value_list l).
+Proof.
+  intros. induction l.
+  * simpl. reflexivity.
+  * simpl. destruct a; try (inversion H).
+    - simpl. rewrite (IHl H1). auto.
+Qed.
+
+Theorem make_list_length :
+  forall {A : Type} e n, length (@make_list A e n) = n.
+Proof.
+  intros. induction n.
+  * simpl. reflexivity.
+  * simpl. rewrite (IHn). auto.
+Qed.
+
+Theorem last_make_list :
+  forall {A : Type} e n, last (@make_list A e n) e = e.
+Proof.
+  intros. induction n.
+  * simpl. reflexivity.
+  * simpl. rewrite (IHn). destruct (make_list e n); auto.
+Qed.
+
+Theorem nth_make_list :
+  forall {A : Type} e def n i, i < n -> nth i (@make_list A e n) def = e.
+Proof.
+  induction n; intros.
+  * inversion H.
+  * simpl. destruct i; auto.
+    - apply lt_S_n in H. apply IHn. auto.
+Qed.
+
+Theorem nth_def_make_list :
+  forall {A : Type} e def n i, i < n -> nth_def (@make_list A e n) e def i = e.
+Proof.
+  intros. unfold nth_def. destruct i.
+  * auto.
+  * apply nth_make_list. omega.
+Qed.
+
+Theorem S_nth_def_make_list :
+  forall {A : Type} e def n i, i < n -> nth_def (@make_list A e n) e def (S i) = e.
+Proof.
+  intros. unfold nth_def. apply nth_make_list. auto.
+Qed.
+
+Theorem quick_tuple_eval :
+  forall l env id eff,
+  is_literal_list l = true
+->
+  |env, id, ETuple l, eff| -e> |id, inl (VTuple (literal_list_to_value_list l)), eff |.
+Proof.
+  intros. eapply eval_tuple with (eff := make_list eff (length l)) (ids := make_list id (length l)).
+  - apply length_lit_list. assumption.
+  - rewrite make_list_length. auto.
+  - rewrite make_list_length. auto.
+  - intros. pose (eval_lit_list l env id eff H i H0).
+    rewrite nth_def_make_list, nth_def_make_list, S_nth_def_make_list, S_nth_def_make_list. all: auto.
+  - apply eq_sym, last_make_list.
+  - apply eq_sym, last_make_list.
+Qed.
+
+Fixpoint check_lit_cons (e : Expression) : bool :=
+match e with
+| ENil => true
+| ECons (ELit x) y => match y with
+                      | ENil => true 
+                      | ECons y' z => check_lit_cons y
+                      | ELit y' => true
+                      | _ => false
+                      end
+| _ => false
+end.
+
+Fixpoint exp_cons_to_val_cons (e : Expression) : Value :=
+match e with
+| ENil => VNil
+| ECons (ELit x) y => match y with
+                      | ENil => VCons (VLit x) VNil 
+                      | ECons y' z => VCons (VLit x) (exp_cons_to_val_cons y)
+                      | ELit y' => VCons (VLit x) (VLit y')
+                      | _ => ErrorValue
+                      end
+| _ => ErrorValue
+end.
+
+Theorem quick_list_eval :
+  forall e env id eff, check_lit_cons e = true
+->
+  | env, id, e, eff | -e> | id, inl (exp_cons_to_val_cons e), eff|.
+Proof.
+  induction e; intros; try (inversion H).
+  * simpl. apply eval_nil.
+  * simpl. destruct e4, e5; try (inversion H1).
+    - eapply eval_cons. apply eval_nil. apply eval_lit.
+    - eapply eval_cons; apply eval_lit.
+    - eapply eval_cons. 2: apply eval_lit.
+      + apply IHe2. assumption.
+Qed.
+
+
 (** Macro tactics *)
 Ltac simpl_app :=
   repeat (rewrite app_assoc);
@@ -68,6 +210,8 @@ match goal with
 | |- | ?env, ?id, EFunId ?fid, ?eff | -e> | ?id', ?res, ?eff'| => finishing_tactic
 | |- | ?env, ?id, EFun ?pl ?b, ?eff | -e> | ?id', ?res, ?eff'| => finishing_tactic
 | |- | ?env, ?id, ETuple ?l, ?eff | -e> | ?id', ?res, ?eff'| =>
+     (apply quick_tuple_eval; simpl; reflexivity)
+     +
      (eapply eval_tuple;
      unfold_list2;
      unfold_elements;
@@ -76,6 +220,8 @@ match goal with
      auto)
      + tuple_exception_solver 0
 | |- | ?env, ?id, ECons _ _, ?eff | -e> | ?id', ?res, ?eff'| =>
+     (apply quick_list_eval; simpl; reflexivity)
+     +
      (eapply eval_cons; solve_inners)
      +
      (eapply eval_cons_tl_ex; solve_inners)
@@ -380,6 +526,16 @@ call_exception_solver num :=
       call_exception_solver (S num)
   end
 .
+
+Example tuple_lit_eval :
+  exists id' v' eff',
+  | [], 0, ETuple [ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok");ELit (Atom "ok"); ELit (Atom "ok"); ELit (Atom "ok")], [] | -e> | id', v', eff' |.
+Proof.
+  eexists. eexists. eexists.
+  solve.
+Qed.
+
+
 
 Definition exception_call : Expression := ECall "+" [ELit (Integer 5); ETuple []].
 
