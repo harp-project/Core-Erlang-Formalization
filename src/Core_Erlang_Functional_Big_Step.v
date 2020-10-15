ff48e37fa3a226f624d0bde323d01594a3fc4a54
berpeti <peti.ber007@gmail.com>
2020-10-15 12:42:17 +0200

    Progress with clock increase (case proved)

--- src/Core_Erlang_Functional_Big_Step.v
+++ src/Core_Erlang_Functional_Big_Step.v
@@ -59,23 +59,7 @@ match clock with
 | 0 => Timeout
 | S clock' =>
   match expr with
-   | EValues el => 
-       (* (fix eval_list env id exps eff : ResultType := 
-                   match exps with
-                   | []    => Result id (inl []) eff
-                   | x::xs => match fbs_single env id x eff clock' with
-                              | Result id' (inl [v]) eff' => 
-                                let res := eval_list env id' xs eff' in
-                                  match res with
-                                  | Result id'' (inl xs') eff'' => Result id'' (inl (v::xs')) eff''
-                                  | r => r
-                                  end
-                              | Result id' (inl val) eff' => Failure (* undefined behaviour *)
-                              | r => r
-                              end
-                   end
-                   ) *) 
-                   fbs_values (fbs_single clock') env id el eff
+   | EValues el => fbs_values (fbs_single clock') env id el eff
    | ESingle e => fbs_single clock' env id e eff
   end
 end
@@ -139,30 +123,31 @@ match clock with
    | ECase e l =>
      match fbs_expr clock' env id e eff with
      | Result id' (inl vals) eff' =>
-       (fix clause_eval l i' :=
-         let i := (length l) - i' in
-           match i' with
-           | 0 => Result id' (inr if_clause) eff'
-           | S i'' =>
-           (* TODO: side effects cannot be produced here *)
-             match match_clause vals l i with
-             | Some (gg, bb, bindings) =>
-               match fbs_expr clock' (add_bindings bindings env) id' gg eff' with
-               | Result id'' (inl [v]) eff'' =>  
-                   match v with
-                   | VLit (Atom "true"%string)  => 
-                      if andb (Nat.eqb id'' id') (list_eqb effect_eqb eff' eff'')
-                      then fbs_expr clock' (add_bindings bindings env) id' bb eff'
-                      else (* undef *) Failure
-                   | VLit (Atom "false"%string) => clause_eval l i''
-                   | _ => Failure
-                   end
-               | _ => Failure
-               end
-             | None => clause_eval l i''
+       (fix clause_eval l :=
+         match l with
+         | [] => Result id' (inr if_clause) eff'
+         | (pl, gg, bb)::xs =>
+         (* TODO: side effects cannot be produced here *)
+           if match_valuelist_to_patternlist vals pl
+           then
+             match fbs_expr clock' (add_bindings (match_valuelist_bind_patternlist vals pl) env) id' gg eff' with
+             | Result id'' (inl [v]) eff'' =>  
+                 match v with
+                 | VLit (Atom s) =>
+                   if String.eqb s "true"%string then
+                     if andb (Nat.eqb id'' id') (list_eqb effect_eqb eff' eff'')
+                     then fbs_expr clock' (add_bindings (match_valuelist_bind_patternlist vals pl) env) id' bb eff'
+                     else (* undef *) Failure
+                   else if String.eqb s "false"%string 
+                   then clause_eval xs
+                   else Failure
+                 | _ => Failure
+                 end
+             | _ => Failure
              end
-           end
-       ) l (length l)
+           else clause_eval xs
+        end
+        ) l
      | r => r
      end
    | ELet l e1 e2 =>
@@ -192,40 +177,6 @@ match clock with
          end
        | r => r
        end
-     (* (fix eval_list_map env id exps eff : MapResultType :=
-       match exps with
-       | [] => MapResult id (inl ([], [])) eff
-       | (key, val)::xs =>
-         match fbs_expr clock' env id key eff with
-         | Result id' (inl [kval]) eff' =>
-            match fbs_expr clock' env id' val eff' with
-            | Result id'' (inl [vval]) eff'' => 
-               let res := eval_list_map env id'' xs eff''
-               in match res with
-               | MapResult id''' (inl (kvals, vvals)) eff''' =>
-                  MapResult id''' (inl (kval::kvals, vval::vvals)) eff'''
-               | r => r
-               end
-            | Result id'' (inl vals) eff'' => MapFailure
-            | Result id'' (inr ex) eff'' => MapResult id'' (inr ex) eff''
-            | Failure => MapFailure
-            | Timeout => MapTimeout
-            end
-         | Result id' (inl vals) eff' => MapFailure
-         | Result id'' (inr ex) eff'' => MapResult id'' (inr ex) eff''
-         | Failure => MapFailure
-         | Timeout => MapTimeout
-         end
-       end
-     ) env id l eff in
-     match res with
-     | MapResult id' (inl (kvals, vvals)) eff' =>
-       let (x, y) := make_value_map kvals vvals
-       in Result id' (inl [VMap (combine x y)]) eff'
-     | MapResult id' (inr ex) eff' => Result id' (inr ex) eff'
-     | MapFailure => Failure
-     | MapTimeout => Timeout
-     end *)
    | ETry e1 vl1 e2 vl2 e3 =>
      match fbs_expr clock' env id e1 eff with
      | Result id' (inl vals) eff' =>
@@ -245,67 +196,6 @@ match res with
 | Result _ (inl [val]) _ => Some val
 | _ => None
 end.
-
-Theorem clock_increase :
-(
-forall clock env id exp eff id' res eff',
-  fbs_expr clock env id exp eff = Result id' res eff'
-->
-  fbs_expr (S clock) env id exp eff = Result id' res eff'
-)
-/\
-(
-forall clock env id exp eff id' res eff',
-  fbs_single clock env id exp eff = Result id' res eff'
-->
-  fbs_single (S clock) env id exp eff = Result id' res eff'
-).
-Proof.
- (* TODO *)
-Admitted.
-
-Theorem clock_increase_expr :
-forall {clock env id exp eff id' res eff'},
-  fbs_expr clock env id exp eff = Result id' res eff'
-->
-  fbs_expr (S clock) env id exp eff = Result id' res eff'.
-Proof.
-  apply clock_increase.
-Qed.
-
-Theorem clock_increase_single :
-forall {clock env id exp eff id' res eff'},
-  fbs_single clock env id exp eff = Result id' res eff'
-->
-  fbs_single (S clock) env id exp eff = Result id' res eff'.
-Proof.
-  apply clock_increase.
-Qed.
-
-Theorem bigger_clock_expr :
-  forall clock clock' env id exp eff id' res eff',
-  clock <= clock' ->
-  fbs_expr clock env id exp eff = Result id' res eff'
-->
-  fbs_expr clock' env id exp eff = Result id' res eff'.
-Proof.
-  intros. induction H.
-  * assumption. 
-  * apply clock_increase. auto.
-Qed.
-
-Theorem bigger_clock_single :
-  forall clock clock' env id exp eff id' res eff',
-  clock <= clock' ->
-  fbs_single clock env id exp eff = Result id' res eff'
-->
-  fbs_single clock' env id exp eff = Result id' res eff'.
-Proof.
-  intros. induction H.
-  * assumption. 
-  * apply clock_increase. auto.
-Qed.
-
 
 Theorem clock_list_increase :
 forall {A:Type} {l env id eff id' res eff' clock} {f : nat -> Environment -> nat -> A -> SideEffectList -> ResultType},
@@ -349,6 +239,204 @@ Proof.
   intros. induction H.
   * assumption.
   * apply clock_list_increase. 2: auto. intros. auto.
+Qed.
+
+Lemma case_clock_increase :
+forall {clock env l id' res eff' id0 eff0 vals},
+(fix clause_eval l :=
+         match l with
+         | [] => Result id0 (inr if_clause) eff0
+         | (pl, gg, bb)::xs =>
+         (* TODO: side effects cannot be produced here *)
+           if match_valuelist_to_patternlist vals pl
+           then
+             match fbs_expr clock (add_bindings (match_valuelist_bind_patternlist vals pl) env) id0 gg eff0 with
+             | Result id'' (inl [v]) eff'' =>  
+                 match v with
+                 | VLit (Atom s) =>
+                   if String.eqb s "true"%string then
+                     if andb (Nat.eqb id'' id0) (list_eqb effect_eqb eff0 eff'')
+                     then fbs_expr clock (add_bindings (match_valuelist_bind_patternlist vals pl) env) id0 bb eff0
+                     else (* undef *) Failure
+                   else if String.eqb s "false"%string 
+                   then clause_eval xs
+                   else Failure
+                 | _ => Failure
+                 end
+             | _ => Failure
+             end
+           else clause_eval xs
+        end
+        ) l = Result id' res eff' ->
+(forall (env : Environment) (id : nat) (exp : Expression) 
+            (eff : SideEffectList) (id' : nat) (res : ValueSequence + Exception)
+            (eff' : SideEffectList),
+          fbs_expr clock env id exp eff = Result id' res eff' ->
+          fbs_expr (S clock) env id exp eff = Result id' res eff')
+->
+(fix clause_eval l :=
+         match l with
+         | [] => Result id0 (inr if_clause) eff0
+         | (pl, gg, bb)::xs =>
+         (* TODO: side effects cannot be produced here *)
+           if match_valuelist_to_patternlist vals pl
+           then
+             match fbs_expr (S clock) (add_bindings (match_valuelist_bind_patternlist vals pl) env) id0 gg eff0 with
+             | Result id'' (inl [v]) eff'' =>  
+                 match v with
+                 | VLit (Atom s) =>
+                   if String.eqb s "true"%string then
+                     if andb (Nat.eqb id'' id0) (list_eqb effect_eqb eff0 eff'')
+                     then fbs_expr (S clock) (add_bindings (match_valuelist_bind_patternlist vals pl) env) id0 bb eff0
+                     else (* undef *) Failure
+                   else if String.eqb s "false"%string 
+                   then clause_eval xs
+                   else Failure
+                 | _ => Failure
+                 end
+             | _ => Failure
+             end
+           else clause_eval xs
+        end
+        ) l = Result id' res eff'.
+Proof.
+  induction l; intros.
+  * simpl in *. auto.
+  * destruct a. destruct p. remember (S clock) as cl.
+    simpl.
+    destruct (match_valuelist_to_patternlist vals l0).
+    - case_eq (fbs_expr clock (add_bindings (match_valuelist_bind_patternlist vals l0) env) id0 e0 eff0);
+         intros; rewrite H1 in H.
+     + apply H0 in H1; rewrite H1. destruct res0.
+       ** destruct v. 2: destruct v0. 1, 3: congruence.
+          destruct v; try congruence.
+          destruct l1; try congruence.
+          destruct (s =? "true")%string.
+          -- destruct (((id =? id0) && list_eqb effect_eqb eff0 eff)%bool).
+            ++ auto.
+            ++ congruence.
+          -- destruct (s =? "false")%string.
+            ++ apply IHl; auto.
+            ++ congruence.
+       ** congruence.
+     + congruence.
+     + congruence.
+   - apply IHl; auto.
+Qed.
+
+Theorem clock_increase_single :
+forall {clock env id exp eff id' res eff'},
+  fbs_single clock env id exp eff = Result id' res eff'
+->
+  fbs_single (S clock) env id exp eff = Result id' res eff'
+with clock_increase_expr :
+forall {clock env id exp eff id' res eff'},
+  fbs_expr clock env id exp eff = Result id' res eff'
+->
+  fbs_expr (S clock) env id exp eff = Result id' res eff'.
+Proof.
+  {
+  induction clock; intros.
+  * simpl in H. inversion H.
+  * simpl in H. destruct exp; intros.
+    1-5: simpl; inversion H; reflexivity.
+    - remember (S clock) as cl. simpl.
+      rewrite Heqcl in *.
+      case_eq (fbs_expr clock env id tl eff); intros.
+      + rewrite H0 in H.
+        apply clock_increase_expr in H0. rewrite H0.
+        destruct res0.
+        ** destruct v. 2: destruct v0. 1, 3: inversion H.
+           case_eq (fbs_expr clock env id0 hd eff0); intros; rewrite H1 in H.
+           -- apply clock_increase_expr in H1. rewrite H1. destruct res0.
+             ++ destruct v0. inversion H. destruct v1. inversion H. auto. inversion H.
+             ++ inversion H. auto.
+           -- inversion H.
+           -- inversion H.
+        ** inversion H. auto.
+      + rewrite H0 in H. inversion H.
+      + rewrite H0 in H. inversion H.
+    - case_eq (fbs_values (fbs_expr clock) env id l eff); intros.
+      + rewrite H0 in H.
+        apply clock_list_increase in H0. 2: auto. remember (S clock) as cl.
+        simpl. rewrite H0. auto.
+      + rewrite H0 in H. congruence.
+      + rewrite H0 in H. congruence.
+    - case_eq (fbs_values (fbs_expr clock) env id l eff); intros.
+      + rewrite H0 in H.
+        apply clock_list_increase in H0. 2: auto. remember (S clock) as cl.
+        simpl. rewrite H0. auto.
+      + rewrite H0 in H. congruence.
+      + rewrite H0 in H. congruence.
+    - case_eq (fbs_values (fbs_expr clock) env id l eff); intros.
+      + rewrite H0 in H.
+        apply clock_list_increase in H0. 2: auto. remember (S clock) as cl.
+        simpl. rewrite H0. auto.
+      + rewrite H0 in H. congruence.
+      + rewrite H0 in H. congruence.
+    - case_eq (fbs_expr clock env id exp eff); intros; rewrite H0 in H.
+      + apply clock_increase_expr in H0. remember (S clock) as cl.
+        simpl. rewrite H0. destruct res0.
+        ** destruct v. 2: destruct v0. 1-3: inversion H.
+           case_eq (fbs_values (fbs_expr clock) env id0 l eff0); intros; rewrite H1 in H.
+           -- apply clock_list_increase in H1. 2: auto. rewrite <- Heqcl in H1. rewrite H1.
+              destruct res0.
+              ++ destruct v; inversion H; auto.
+                 destruct (Datatypes.length vl =? Datatypes.length v0).
+                 rewrite H.
+                 apply clock_increase_expr in H. rewrite <- Heqcl in H. rewrite H.
+                 auto. auto.
+              ++ auto.
+           -- congruence.
+           -- congruence.
+        ** inversion H. auto.
+      + congruence.
+      + congruence.
+    - case_eq (fbs_expr clock env id e eff); intros; rewrite H0 in H.
+      + apply clock_increase_expr in H0. remember (S clock) as cl.
+        simpl. rewrite H0. destruct res0.
+        ** apply case_clock_increase in H. rewrite <- Heqcl in H.
+           auto. auto.
+        ** inversion H. auto.
+      + congruence.
+      + congruence.
+    - admit.
+    - admit.
+    - admit.
+    - admit.
+    - admit.
+  }
+  {
+    induction clock.
+    * intros. simpl in H. congruence.
+    * intros. simpl in H. destruct exp.
+      - apply clock_list_increase in H. remember (S clock) as cl. simpl. auto. auto.
+      - apply clock_increase_single in H. remember (S clock) as cl. simpl. auto.
+  }
+Qed.
+
+Theorem bigger_clock_expr :
+  forall clock clock' env id exp eff id' res eff',
+  clock <= clock' ->
+  fbs_expr clock env id exp eff = Result id' res eff'
+->
+  fbs_expr clock' env id exp eff = Result id' res eff'.
+Proof.
+  intros. induction H.
+  * assumption. 
+  * apply clock_increase_expr in H0. auto.
+Qed.
+
+Theorem bigger_clock_single :
+  forall clock clock' env id exp eff id' res eff',
+  clock <= clock' ->
+  fbs_single clock env id exp eff = Result id' res eff'
+->
+  fbs_single clock' env id exp eff = Result id' res eff'.
+Proof.
+  intros. induction H.
+  * assumption. 
+  * apply clock_increase. auto.
 Qed.
 
 
