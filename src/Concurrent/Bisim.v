dc2533739f942038fe65e3170583daaeb882ca71
berpeti <peti.ber007@gmail.com>
2023-09-27 12:56:39 +0200

    First def for simulations

--- src/Concurrent/Bisim.v
+++ src/Concurrent/Bisim.v
@@ -7,3 +7,59 @@
   -
 
 *)
+
+From CoreErlang Require Import Concurrent.NodeSemantics.
+
+Import ListNotations.
+
+(* Definition dom (Π : ProcessPool) (l : list PID) : Prop :=
+  forall ι, ((Π ι <> None)%type -> In ι l) /\
+            (Π ι = None -> ~In ι l). *)
+
+Definition isUnTaken (ι : PID) (Π : ProcessPool) : Prop :=
+  Π ι = None.
+
+Definition isPreCompatible (Π₁ Π₂ : ProcessPool) : Prop :=
+  forall ι, isUnTaken ι Π₁ -> isUnTaken ι Π₂.
+
+(* Definition isCompatible (Π₁ Π₂ : ProcessPool) : Prop :=
+  isPreCompatible Π₁ Π₂ /\ isPreCompatible Π₂ Π₁. *)
+
+
+(** This is needed to ensure that a process is not spawned with a wrong PID
+    in the equivalence. *)
+Definition PIDOf (a : Action) : option PID :=
+  match a with
+  | ASpawn ι _ _ => Some ι
+  | _ => None
+  end.
+
+(* TODO: do this with Applicative/Monad *)
+Fixpoint PIDsOf (l : list (Action * PID)) : list PID :=
+  match l with
+  | [] => []
+  | (a, _) :: xs =>
+    match PIDOf a with
+    | Some ι => [ι]
+    | None => []
+    end ++ PIDsOf xs
+  end.
+
+Definition isPreCompatibleReductions
+  (n1 n2 : Node) l
+  : Prop :=
+  isPreCompatible (snd n1) (snd n2) /\
+  Forall (fun ι' => isUnTaken ι' (snd n2)) (PIDsOf l).
+
+(* Definition isCompatibleReduction
+  {Π₁ Π₂ Π₁' Π₂' : ProcessPool} {e1 e2 e1' e2' : Ether} {l l'}
+  (red1 : (e1, Π₁) -[l]ₙ->* (e1', Π₁'))
+  (red2 : (e2, Π₂) -[l']ₙ->* (e2', Π₂'))
+  : Prop :=
+  isPreCompatibleReductions red1 red2 /\
+  isPreCompatibleReductions red2 red1. *)
+
+(* This does not say anything about the equivalence of actions *)
+Definition simulates (S : Node -> Node -> Prop) :=
+  forall n₁ n₁' n₂ a ι (pf : S n₁ n₂) (red : n₁ -[a | ι]ₙ-> n₁'),
+    exists n₂' l, n₂ -[l]ₙ->* n₂' /\ isPreCompatibleReductions n₁ n₂ l /\ S n₁' n₂'.
--- src/Concurrent/NodeSemantics.v
+++ src/Concurrent/NodeSemantics.v
@@ -151,7 +151,7 @@ where "n -[ a | ι ]ₙ-> n'" := (nodeSemantics n a ι n').
 
 (** Refexive, transitive closure, with action logs: *)
 Reserved Notation "n -[ l ]ₙ->* n'" (at level 50).
-Inductive closureNodeSem : Node -> list (Action * PID) -> Node -> Prop :=
+CoInductive closureNodeSem : Node -> list (Action * PID) -> Node -> Prop :=
 | n_refl n (* n'  *): (* Permutation n n' -> *) n -[ [] ]ₙ->* n (* ' *)
 | n_trans n n' n'' l a ι:
   n -[a|ι]ₙ-> n' -> n' -[l]ₙ->* n''
@@ -164,8 +164,9 @@ Theorem closureNodeSem_trans :
 ->
   n -[l ++ l']ₙ->* n''.
 Proof.
-  intros n n' l D1. induction D1; intros; simpl.
+  cofix IHD1; intros n n' l D1; inv D1; intros; simpl.
   * exact H.
   * eapply n_trans. exact H.
-    eapply IHD1 in H0. exact H0.
+    eapply (IHD1 _ _ _ H0) in H1. exact H1.
 Qed.
+
