2f7b7c66c31a4280e44f2eaa2e919c823e4dfc80
berpeti <peti.ber007@gmail.com>
2023-10-13 16:03:26 +0200

    Progress with bisim properties + coiductive defs of expansion, bisim up-to

--- src/Concurrent/BarbedBisim.v
+++ src/Concurrent/BarbedBisim.v
@@ -116,11 +116,6 @@ end.
 Definition isUnTaken (ι : PID) (n : Node) : Prop :=
   n.2 ι = None /\ isUsed ι n.1.
 
-(* This relation is not transitive unfortunately, since isUsed is not
-   in the conclusion *)
-Definition preCompatibleNodes (n1 n2 : Node) : Prop :=
-  forall ι, isUnTaken ι n1 -> n2.2 ι = None.
-
 Definition symClos {T : Type} (R : T -> T -> Prop) : T -> T -> Prop :=
   fun t1 t2 => R t1 t2 /\ R t2 t1.
 
@@ -129,7 +124,7 @@ Theorem compatibility_of_reduction :
     forall ι, isUnTaken ι n ->
       (isUnTaken ι n' \/ spawnPIDOf a = Some ι /\ exists p, n'.2 ι = Some p).
 Proof.
-  intros. inv H; unfold symClos, preCompatibleNodes.
+  intros. inv H; unfold symClos.
   * left. destruct H0. split.
     - simpl in *. unfold update in *; now break_match_goal.
     - simpl in *. now apply isUsed_etherAdd.
@@ -177,7 +172,7 @@ Theorem compatibility_of_reduction_rev :
       (sendPIDOf a = Some ι /\ ~isUsed ι n.1 /\ n.2 ι = None).
 (*       (spawnPIDOf a = Some ι /\ n.2 ι = None). *)
 Proof.
-  intros. inv H; unfold symClos, preCompatibleNodes.
+  intros. inv H; unfold symClos.
   * destruct H0.
     destruct (Nat.eq_dec ι' ι0); simpl; subst.
     - simpl in *.
@@ -238,41 +233,6 @@ Proof.
         apply in_or_app. simpl. right. rewrite H0. now constructor.
 Qed.
 
-Fixpoint overtakes {A : Type} (eq_dec :forall (a b : A), {a = b} + {a <> b}) (a b : A) (l : list A) : Prop :=
-  match l with
-  | [] => False
-  | x :: xs =>
-    match eq_dec a x with
-    | left _ => True
-    | right _ => match eq_dec b x with
-                 | left _ => False
-                 | right _ => overtakes eq_dec a b xs
-                 end
-    end
-  end.
-
-(* Lemma spawn_overtakes_send :
-  forall n n' l, n -[l]ₙ->* n' ->
-    forall ι, In ι (PIDsOf sendPIDOf l) -> In ι (PIDsOf spawnPIDOf l) ->
-      overtakes (ι, spawn) (ι, send) l. *)
-
-(* What if a new PID is targeted by a message ->
-   PID compatibility should also include sent messages 
-
-   The following definition is sufficient, because if both spawn and send
-   is present in the trace for some PID ι, then the spawn will happen first
-   always. If there is no spawn (only send) for ι, then this should hold for
-   the other trace.
-*)
-Definition reductionPreCompatibility (n1 n2 : Node) l l' :=
-  Forall (fun x => ~isUnTaken x n2) (PIDsOf spawnPIDOf l) /\
-  (forall ι, n1.2 ι = None ->
-             In ι (PIDsOf sendPIDOf l) -> 
-             ~In ι (PIDsOf spawnPIDOf l) ->
-        n2.2 ι = None /\
-        In ι (PIDsOf sendPIDOf l') /\
-        ~In ι (PIDsOf spawnPIDOf l')).
-
 Lemma no_spawn_unTaken :
   forall n n' l, n -[l]ₙ->* n' ->
     forall ι, isUnTaken ι n ->
@@ -322,6 +282,46 @@ Proof.
       subst. apply processes_dont_die with (ι := ι0) in H0; auto.
       cbn. unfold update. now rewrite Nat.eqb_refl.
 Qed.
+
+Fixpoint overtakes {A : Type} (eq_dec :forall (a b : A), {a = b} + {a <> b}) (a b : A) (l : list A) : Prop :=
+  match l with
+  | [] => False
+  | x :: xs =>
+    match eq_dec a x with
+    | left _ => True
+    | right _ => match eq_dec b x with
+                 | left _ => False
+                 | right _ => overtakes eq_dec a b xs
+                 end
+    end
+  end.
+
+(* Lemma spawn_overtakes_send :
+  forall n n' l, n -[l]ₙ->* n' ->
+    forall ι, In ι (PIDsOf sendPIDOf l) -> In ι (PIDsOf spawnPIDOf l) ->
+      overtakes (ι, spawn) (ι, send) l. *)
+
+(* What if a new PID is targeted by a message ->
+   PID compatibility should also include sent messages 
+
+   The following definition is sufficient, because if both spawn and send
+   is present in the trace for some PID ι, then the spawn will happen first
+   always. If there is no spawn (only send) for ι, then this should hold for
+   the other trace.
+*)
+Definition reductionPreCompatibility (n1 n2 : Node) l l' :=
+  Forall (fun x => ~isUnTaken x n2) (PIDsOf spawnPIDOf l) /\
+  (forall ι, n1.2 ι = None ->
+             In ι (PIDsOf sendPIDOf l) -> 
+             ~In ι (PIDsOf spawnPIDOf l) ->
+        n2.2 ι = None /\
+        In ι (PIDsOf sendPIDOf l') /\
+        ~In ι (PIDsOf spawnPIDOf l')).
+
+(* This relation is not transitive unfortunately, since isUsed is not
+   in the conclusion *)
+Definition preCompatibleNodes (n1 n2 : Node) : Prop :=
+  forall ι, isUnTaken ι n1 -> n2.2 ι = None.
 
 Theorem reduction_preserves_preCompatibility :
   forall n1 n2, (* symClos *) preCompatibleNodes n1 n2 ->
@@ -393,6 +393,7 @@ CoInductive barbedBisim (U : list PID) : Node -> Node -> Prop :=
       list_biforall Srel (B.1 source dest) (A'.1 source' dest)) ->
   barbedBisim U A B
 .
+Notation "A ~ B 'using' U" := (barbedBisim U A B) (at level 70).
 
 Theorem reductionPreCompatibility_refl A A' a ι:
   A -[ a | ι ]ₙ-> A' ->
@@ -414,9 +415,8 @@ Proof.
   destruct s; simpl; auto.
 Qed.
 
-(* Co-induction test, this probably can be proven without it *)
 Theorem barbedBisim_refl :
-  forall U A, ether_wf A.1 -> barbedBisim U A A.
+  forall U A, ether_wf A.1 -> A ~ A using U.
 Proof.
   cofix H. intros.
   constructor.
@@ -446,83 +446,168 @@ Proof.
     intros. apply Srel_refl. now apply H0.
 Qed.
 
-Definition barbedExpansion (U : list PID) (S : Node -> Node -> Prop) :=
-  forall (A B : Node), S A B ->
+Theorem barbedBisim_sym :
+  forall U A B, A ~ B using U -> B ~ A using U.
+Proof.
+  cofix IH.
+  intros. inv H. constructor; auto.
+  * split; apply H0.
+  * clear H6 H4. intros.
+    apply H5 in H. destruct_hyps.
+    do 2 eexists. split. 2: split. 3: split. 3: eassumption.
+    1-2: auto.
+    now apply IH.
+  * clear H6 H4. intros.
+    apply H3 in H. destruct_hyps.
+    do 2 eexists. split. 2: split. 3: split. 3: eassumption.
+    1-2: auto.
+    now apply IH.
+Qed.
+
+(* Lemma preCompatibleNodes_trans :
+  forall A B C, preCompatibleNodes A B -> preCompatibleNodes B C ->
+    preCompatibleNodes A C.
+Proof.
+  intros. intros ι Hi. apply H in Hi.
+Qed. *)
+
+Lemma barbedBisim_many :
+  forall A A' l, A -[l]ₙ->* A' ->
+    forall U B, A ~ B using U ->
+      exists B' l',
+        reductionPreCompatibility A B l l' /\
+        reductionPreCompatibility B A l' l /\
+        B -[ l' ]ₙ->* B' /\ A' ~ B' using U.
+Proof.
+  intros A A' l IH. induction IH; rename n into A; intros.
+  * exists B, []. split. 2: split. 3: split. 3: constructor. 3: assumption.
+    1-2: split; constructor; auto.
+    1-2: inv H1.
+  * rename n' into A'. rename n'' into A''.
+    inv H0. apply H4 in H. destruct H as [B' [l' H]]. destruct_hyps.
+    clear H4 H5 H6 H7. apply IHIH in H9. destruct H9 as [B'' [l'' H9]].
+    destruct_hyps.
+    exists B'', (l' ++ l''). split. 2: split. 3: split.
+    4: assumption.
+    3: eapply closureNodeSem_trans; eassumption.
+    Check reduction_preserves_preCompatibility.
+Qed.
+
+Lemma reductionPreCompatibility_app :
+  forall xs xs' A B,
+    reductionPreCompatibility A B xs xs' ->
+    reductionPreCompatibility B A xs' xs ->
+    forall ys ys' A' B' (* B'' A'' *),
+      A -[xs]ₙ->* A' ->
+      (* A' -[ys]ₙ->* A'' -> *)
+      B -[xs']ₙ->* B' ->
+      (* B' -[ys']ₙ->* B'' -> *)
+      reductionPreCompatibility A' B' ys ys' ->
+      reductionPreCompatibility B' A' ys' ys ->
+      reductionPreCompatibility A B (xs ++ ys) (xs' ++ ys').
+Proof.
+  intros. unfold reductionPreCompatibility in *. destruct_hyps.
+  split.
+  * unfold PIDsOf. rewrite flat_map_app. fold (PIDsOf spawnPIDOf xs).
+    fold (PIDsOf spawnPIDOf ys). apply Forall_app; split; auto.
+    Search closureNodeSem isUnTaken.
+    clear H5 H6 H8 H7.
+    (** Proof by contradiction :
+        If ι ∈ (PIDsOf spawnPIDOf ys) 
+        1) were used (isUsed ι B), then it couldn't be contained in
+           this list (spawn could not happen on it).
+        2) were assigned in B (B.2 ι = Some proc), then spawn could
+           not happen on it
+    *)
+    rewrite Forall_forall in *. intros. apply H3 in H5 as H5'. clear H3.
+    intro.
+    eapply compatibility_of_reductions in H2 as H2'. 2: eassumption.
+    destruct H2'. congruence. destruct_hyps.
+    apply H0 in H6 as H6'.
+
+Qed.
+
+Theorem barbedBisim_trans :
+  forall U A B C, A ~ B using U -> B ~ C using U -> A ~ C using U.
+Proof.
+  cofix IH. intros.
+  inv H. inv H0. constructor; auto.
+  * admit.
+  * clear H13 H7. intros. apply H4 in H0.
+    destruct H0 as [B' [l [H0]]]. destruct_hyps.
+
+Qed.
+
+CoInductive barbedExpansion (U : list PID) : Node -> Node -> Prop :=
+| is_expansion A B:
+(*   symClos preCompatibleNodes A B ->
+  ether_wf A.1 -> ether_wf B.1 -> *)
   (forall A' a ι, A -[a | ι]ₙ-> A' -> exists B' l, 
     reductionPreCompatibility A B [(a,ι)] l /\
     reductionPreCompatibility B A l [(a,ι)] /\
-    length l <= 1 /\ B -[l]ₙ->* B' /\ S A' B')
-/\
+    length l <= 1 /\ B -[l]ₙ->* B' /\ barbedExpansion U A' B')
+  ->
   (forall B' a ι, B -[a | ι]ₙ-> B' -> exists A' l,
     reductionPreCompatibility B A [(a,ι)] l /\
     reductionPreCompatibility A B l [(a,ι)] /\
-    length l >= 1 /\ A -[l]ₙ->* A' /\ S A' B')
-/\
+    length l >= 1 /\ A -[l]ₙ->* A' /\ barbedExpansion U A' B')
+  ->
   (forall source dest,
     ~In dest U ->
     exists source', list_biforall Srel (A.1 source dest) (B.1 source' dest))
-/\
+  ->
   (forall source dest,
     ~In dest U ->
     exists source' l A',
     A -[l]ₙ->* A' /\
-    list_biforall Srel (B.1 source dest) (A'.1 source' dest)).
+    list_biforall Srel (B.1 source dest) (A'.1 source' dest))
+->
+  barbedExpansion U A B.
 
-Theorem barbedExpansion_transitive :
-  forall U S, barbedExpansion U S ->
-    forall A B C, S A B -> S B C -> S A C.
-Proof.
-  intros. apply H in H0. apply H in H1.
-  clear H. destruct_hyps.
-Qed.
+Notation "A ⪯ B 'using' U" := (barbedExpansion U A B) (at level 70).
 
-Definition barbedBisimUpTo (U : list PID) (S : Node -> Node -> Prop) :=
- barbedExpansion U S /\
- forall A B, S A B ->
+CoInductive barbedBisimUpTo (U : list PID) : Node -> Node -> Prop :=
+| is_bisim_up_to A B:
+  symClos preCompatibleNodes A B ->
+  ether_wf A.1 -> ether_wf B.1 ->
   (forall A' a ι, A -[a | ι]ₙ-> A' ->
      exists B' A'' B'' l,
        reductionPreCompatibility A B [(a,ι)] l /\
        reductionPreCompatibility B A l [(a,ι)] /\
        B -[l]ₙ->* B' /\
-       S A' A'' /\ S B' B'' /\ S A'' B'') /\
+       A' ⪯ A'' using U /\ B' ⪯ B'' using U /\ barbedBisimUpTo U A'' B'') ->
   (forall source dest,
     ~In dest U ->
     exists source' l B',
     B -[l]ₙ->* B' /\
-    list_biforall Srel (A.1 source dest) (B'.1 source' dest)) /\
+    list_biforall Srel (A.1 source dest) (B'.1 source' dest)) ->
   (forall B' a ι, B -[a | ι]ₙ-> B' ->
      exists A' B'' A'' l,
        reductionPreCompatibility B A [(a,ι)] l /\
        reductionPreCompatibility A B l [(a,ι)] /\
        A -[l]ₙ->* A' /\
-       S A' A'' /\ S B' B'' /\ S A'' B'') /\
+       A' ⪯ A'' using U /\ B' ⪯ B'' using U /\ barbedBisimUpTo U  A'' B'') ->
    (forall source dest,
     ~In dest U ->
     exists source' l A',
     A -[l]ₙ->* A' /\
-    list_biforall Srel (B.1 source dest) (A'.1 source' dest)).
+    list_biforall Srel (B.1 source dest) (A'.1 source' dest))
+->
+  barbedBisimUpTo U A B.
+
+Notation "A ~⪯~ B 'using' U" := (barbedBisimUpTo U A B) (at level 70).
 
 Theorem barbedBisimUpTo_barbedBisim :
-  forall U S, barbedBisimUpTo U S -> barbedBisim U S.
+  forall U A B, A ~⪯~ B using U -> A ~ B using U.
 Proof.
-  unfold barbedBisimUpTo, barbedBisim, symClos. intros.
-  destruct_hyps. split.
-  {
-    intros.
-    apply H3 in H1 as H1'. destruct_hyps.
-    apply H in H1. destruct_hyps.
-    clear H3 H10 H11 H8 H6.
-    apply H1 in H4 as H4'. destruct H4' as [B' [l ?]]. destruct_hyps.
-    apply H5 in H4 as H4'. destruct H4' as [B'2 [A'' [B'' [l2 ?]]]]. destruct_hyps.
+  cofix IH. intros. inv H. constructor; auto.
+  * clear H4 H6 H5. intros. apply H3 in H as H'.
+    destruct H' as [B' [A'' [B'' [l H']]]]. destruct_hyps.
+    inv H7. inv H8. clear H15 H16 H14 H12 H13 H11.
+    apply H10
     
+    apply IH in H9.
     
-    exists n2', l'. do 3 (split; auto).
-      
-    }
-    {
-    
-    }
-  }
 Qed.
 
 
