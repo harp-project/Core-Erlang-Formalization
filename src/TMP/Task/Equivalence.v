2760eb39795f7e8ade311c1b2992e0116deee3ed
Peter Miskolczi <72017244+breadking99@users.noreply.github.com>
2024-04-16 11:15:00 +0200

    valueToExpression optional partial

--- src/TMP/Task/Equivalence.v
+++ src/TMP/Task/Equivalence.v
@@ -1,38 +1,135 @@
 From CoreErlang.BigStep Require Import BigStep.
 
-Import BigStep.
+
 
 Module SubstBigStep.
 
 
-(*To do: valuetoExpreton (option expresion) *)
+Import BigStep.
+Import ListNotations.
 
-Fixpoint valueSequenceToExpression (vs : list Value) : Expression :=
-match vs with
- | [] => ENil
- | v :: vs' => match v with
-               | VNil => ENil
-               | VLit l => ECons (ELit l) (valueSequenceToExpression vs')
-               | VClos env ext id vl e => ENil
-               | VCons hd tl => ECons (ECons (valueSequenceToExpression [hd]) (valueSequenceToExpression [tl])) (valueSequenceToExpression vs')
-               | VTuple l => ECons (ETuple (map valueSequenceToExpression l)) (valueSequenceToExpression vs')
-               | VMap l => ECons (EMap (map (fun '(x, y) => (valueSequenceToExpression [x], valueSequenceToExpression [y])) l)) (alueSequenceToExpression vs')
+
+
+Fixpoint mapOption {A B} (f : A -> nat -> option B) (l : list A) (fuel : nat) : option (list B) :=
+  match fuel with
+  | 0 => None
+  | S fuel' =>  match l with
+                | [] => Some []
+                | x :: xs => match f x fuel' with
+                            | Some y => match mapOption f xs fuel' with
+                                        | Some ys => Some (y :: ys)
+                                        | None => None
+                                        end
+                            | None => None
+                            end
                 end
-end.
+  end.
+
+
+(*Definition mapNone {A} (n : nat) : option (A) := None. *)
+
+
+
+Fixpoint valueToExpression1 (v : Value) (fuel : nat) : option Expression :=
+  match fuel with
+  | 0 => None
+  | S fuel' =>  match v with
+                | VNil => Some ENil
+                | VLit l => Some (ELit l)
+
+                (*TODO*)
+                | VClos env ext id vl e => None
+
+                | VCons hd tl => match (valueToExpression1 hd fuel'), (valueToExpression1 tl fuel') with
+                                | Some hd', Some tl' => Some (ECons hd' tl')
+                                | _, _ => None
+                                end
+                
+                (*TODO*)
+                | VTuple l => None(* match (mapOption valueToExpression l fuel') with
+                              | Some l' => Some (ETuple l')
+                              | None => None
+                              end *)
+                    (*(ETuple (map valueToExpression l))*)
+
+                (*TODO*)
+                | VMap l =>  None (*match (mapOption (fun '(x, y) => match fuel' with
+                                                              | 0 => mapNone 
+                                                              | S fuel'' => match (valueToExpression x fuel'') with
+                                                                            | Some x' => match (valueToExpression y fuel'') with
+                                                                                        | Some y' => Some (x', y')
+                                                                                        | None => None
+                                                                                        end
+                                                                            end
+                                                              end
+
+                                                              (* match (valueToExpression x fuel') with
+                                                              | Some x' => match (valueToExpression y fuel') with
+                                                                          | Some y' => Some (x', y')
+                                                                          | None => None
+                                                                          end
+                                                              end *)
+
+                                                              (*match (valueToExpression x fuel'), (valueToExpression y fuel') with
+                                                              | Some x', Some y' => Some (x', y')
+                                                              | _, _ => None
+                                                              end *)
+                                            ) l fuel') with
+                            | Some l' => Some (EMap l')
+                            | None => None
+                            end
+                            *)
+                    (* EMap (map (fun '(x, y) => (valueToExpression x, valueToExpression y)) l) *)
+                end
+  end.
+
+
+
+Fixpoint valueToExpression (v : Value) : option Expression :=
+  match v with
+  | VNil => Some ENil
+  | VLit l => Some (ELit l)
+
+  (*TODO*)
+  | VClos env ext id vl e => None
+
+  | VCons hd tl => match (valueToExpression hd), (valueToExpression tl) with
+                  | Some hd', Some tl' => Some (ECons hd' tl')
+                  | _, _ => None
+                  end
+  
+  (*TODO*)
+  | VTuple l => None
+      (*(ETuple (map valueToExpression l))*)
+
+  (*TODO*)
+  | VMap l =>  None 
+      (* EMap (map (fun '(x, y) => (valueToExpression x, valueToExpression y)) l) *)
+  end.
+
+
 
 Fixpoint substB (e : Expression) (Γ : Environment) : Expression :=
 match e with
   | EValues el => EValues (map (fun x => substB x Γ) el)
   | ENil => ENil
   | ELit l => ELit l
-  | EVar v => match (get_value Γ (inl v)) with
-              | Some n => valueSequenceToExpression n
+  (*TODO*)
+  | EVar v => EVar v(*match (get_value Γ (inl v)) with
+              | Some n => match (map valueToExpression n) with
+                          | Some e => e
+                          | None => EVar v
+                          end
               | None => EVar v
-              end
-  | EFunId f => match (get_value Γ f) with
-                | Some n => n
+              end *)
+  (*TODO*)
+  | EFunId f => EFunId f (* match (get_value Γ (inr f)) with
+                | Some n => match (map valueToExpression n) with
+                          | Some e => e
+                          | None => EFunId f
+                          end
                 | None => EFunId f
-                end
+                end*)
   | EFun vl e => EFun vl (substB e Γ)
   | ECons hd tl => ECons (substB hd Γ) (substB tl Γ)
   | ETuple l => ETuple (map (fun x => substB x Γ) l)
@@ -40,18 +137,19 @@ match e with
   | EPrimOp f l => EPrimOp f (map (fun x => substB x Γ) l)
   | EApp exp l => EApp (substB exp Γ) (map (fun x => substB x Γ) l)
   | ECase e l => ECase (substB e Γ) (map (fun '(pl, g, b) =>
-                                      (pl, substB g (addVars (varsOfPatternList pl) Γ), substB b (addVars (varsOfPatternList pl) Γ))
-                                      ) l)
-  | ELet l e1 e2 => ELet l (substB e1 Γ) (substB e2 (addVars l Γ))
+                                      (pl, substB g Γ, substB b Γ)
+                                    ) l)
+  | ELet l e1 e2 => ELet l (substB e1 Γ) (substB e2 Γ)
   | ESeq e1 e2 => ESeq (substB e1 Γ) (substB e2 Γ)
   | ELetRec l e => ELetRec (map (fun '(fid, (vl, b)) =>
-                                            (vl, substB b (addNames (map (inr ∘ fst) l ++ map inl vl) Γ))
+                                            ((fid, (vl, substB b Γ)))
                                           ) l)
-                                            (substB e (addFids (map fst l) Γ))
+                                            (substB e Γ)
   | EMap l => EMap (map (fun '(x, y) => (substB x Γ, substB y Γ)) l)
   | ETry e1 vl1 e2 vl2 e0 => ETry (substB e1 Γ)
-                                                  vl1 (substB e2 (addVars vl1 Γ))
-                                                  vl2 (substB e0 (addVars vl1 Γ))
+                                                vl1 (substB e2 Γ)
+                                                vl2 (substB e0 Γ)
   end.
+  
   
   End SubstBigStep.
