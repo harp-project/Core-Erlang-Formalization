28f93870227c03f892c623ed00900c5c70caba7b
Peter Miskolczi <72017244+breadking99@users.noreply.github.com>
2024-04-22 20:49:28 +0200

    option map fix & make subst_bigstep optional
    Renamed valueToExpression to value_to_expression and substB to subst_bigstep.
    Instead mapOption I use (sequence . map), sequence is defined by me.
    I made subst_bistep return option Expression.

--- src/TMP/Task/Equivalence.v
+++ src/TMP/Task/Equivalence.v
@@ -1,155 +1,140 @@
 From CoreErlang.BigStep Require Import BigStep.
+Require Import Coq.Lists.List.
 
 
-
-Module SubstBigStep.
+Module subst_bigstepigStep.
 
 
 Import BigStep.
 Import ListNotations.
 
 
+Search ((_ -> option _) -> _ -> _ option).
+Search "map".
+(*Needed: {A B : Type} (f : A -> option B) (l : list A) : option (list B)*)
 
-Fixpoint mapOption {A B} (f : A -> nat -> option B) (l : list A) (fuel : nat) : option (list B) :=
-  match fuel with
-  | 0 => None
-  | S fuel' =>  match l with
-                | [] => Some []
-                | x :: xs => match f x fuel' with
-                            | Some y => match mapOption f xs fuel' with
-                                        | Some ys => Some (y :: ys)
-                                        | None => None
-                                        end
-                            | None => None
-                            end
-                end
+Fixpoint sequence {A : Type} (l : list (option A)) : option (list A) :=
+  match l with
+  | [] => Some []
+  | hd :: tl => match hd with
+              | Some x => match sequence tl with
+                          | Some xs => Some (x :: xs)
+                          | None => None
+                          end
+              | None => None
+              end
   end.
 
-
-(*Definition mapNone {A} (n : nat) : option (A) := None. *)
-
-
-
-Fixpoint valueToExpression1 (v : Value) (fuel : nat) : option Expression :=
-  match fuel with
-  | 0 => None
-  | S fuel' =>  match v with
-                | VNil => Some ENil
-                | VLit l => Some (ELit l)
-
-                (*TODO*)
-                | VClos env ext id vl e => None
-
-                | VCons hd tl => match (valueToExpression1 hd fuel'), (valueToExpression1 tl fuel') with
-                                | Some hd', Some tl' => Some (ECons hd' tl')
-                                | _, _ => None
-                                end
-                
-                (*TODO*)
-                | VTuple l => None(* match (mapOption valueToExpression l fuel') with
-                              | Some l' => Some (ETuple l')
-                              | None => None
-                              end *)
-                    (*(ETuple (map valueToExpression l))*)
-
-                (*TODO*)
-                | VMap l =>  None (*match (mapOption (fun '(x, y) => match fuel' with
-                                                              | 0 => mapNone 
-                                                              | S fuel'' => match (valueToExpression x fuel'') with
-                                                                            | Some x' => match (valueToExpression y fuel'') with
-                                                                                        | Some y' => Some (x', y')
-                                                                                        | None => None
-                                                                                        end
-                                                                            end
-                                                              end
-
-                                                              (* match (valueToExpression x fuel') with
-                                                              | Some x' => match (valueToExpression y fuel') with
-                                                                          | Some y' => Some (x', y')
-                                                                          | None => None
-                                                                          end
-                                                              end *)
-
-                                                              (*match (valueToExpression x fuel'), (valueToExpression y fuel') with
-                                                              | Some x', Some y' => Some (x', y')
-                                                              | _, _ => None
-                                                              end *)
-                                            ) l fuel') with
-                            | Some l' => Some (EMap l')
-                            | None => None
-                            end
-                            *)
-                    (* EMap (map (fun '(x, y) => (valueToExpression x, valueToExpression y)) l) *)
-                end
-  end.
-
-
-
-Fixpoint valueToExpression (v : Value) : option Expression :=
+Fixpoint value_to_expression (v : Value) : option Expression :=
   match v with
   | VNil => Some ENil
   | VLit l => Some (ELit l)
-
   (*TODO*)
-  | VClos env ext id vl e => None
-
-  | VCons hd tl => match (valueToExpression hd), (valueToExpression tl) with
+  | VClos env ext id vl e => None(*match (subst_bigstep (EFun vl e) env) with
+                              | Some e' => Some (EFun vl e')
+                              | None => None
+                              end*)
+  | VCons hd tl => match (value_to_expression hd), (value_to_expression tl) with
                   | Some hd', Some tl' => Some (ECons hd' tl')
                   | _, _ => None
                   end
-  
-  (*TODO*)
-  | VTuple l => None
-      (*(ETuple (map valueToExpression l))*)
-
-  (*TODO*)
-  | VMap l =>  None 
-      (* EMap (map (fun '(x, y) => (valueToExpression x, valueToExpression y)) l) *)
+  | VTuple l => match (sequence (map value_to_expression l)) with
+                | Some l' => Some (ETuple  l')
+                | None => None
+                end
+  | VMap l =>  match (sequence (map (fun '(x, y) => match (value_to_expression x), (value_to_expression y) with
+                                                    | Some x', Some y' => Some (x', y')
+                                                    | _, _ => None
+                                                    end
+                                                    ) l)) with
+              | Some l' => Some (EMap l')
+              | None => None
+              end
   end.
 
 
-
-Fixpoint substB (e : Expression) (Γ : Environment) : Expression :=
+(*with*) Fixpoint subst_bigstep (e : Expression) (Γ : Environment) : option Expression :=
 match e with
-  | EValues el => EValues (map (fun x => substB x Γ) el)
-  | ENil => ENil
-  | ELit l => ELit l
-  (*TODO*)
-  | EVar v => EVar v(*match (get_value Γ (inl v)) with
-              | Some n => match (map valueToExpression n) with
-                          | Some e => e
-                          | None => EVar v
-                          end
-              | None => EVar v
-              end *)
-  (*TODO*)
-  | EFunId f => EFunId f (* match (get_value Γ (inr f)) with
-                | Some n => match (map valueToExpression n) with
-                          | Some e => e
-                          | None => EFunId f
-                          end
-                | None => EFunId f
-                end*)
-  | EFun vl e => EFun vl (substB e Γ)
-  | ECons hd tl => ECons (substB hd Γ) (substB tl Γ)
-  | ETuple l => ETuple (map (fun x => substB x Γ) l)
-  | ECall m f l => ECall (substB m Γ) (substB f Γ) (map (fun x => substB x Γ) l)
-  | EPrimOp f l => EPrimOp f (map (fun x => substB x Γ) l)
-  | EApp exp l => EApp (substB exp Γ) (map (fun x => substB x Γ) l)
-  | ECase e l => ECase (substB e Γ) (map (fun '(pl, g, b) =>
-                                      (pl, substB g Γ, substB b Γ)
-                                    ) l)
-  | ELet l e1 e2 => ELet l (substB e1 Γ) (substB e2 Γ)
-  | ESeq e1 e2 => ESeq (substB e1 Γ) (substB e2 Γ)
-  | ELetRec l e => ELetRec (map (fun '(fid, (vl, b)) =>
-                                            ((fid, (vl, substB b Γ)))
-                                          ) l)
-                                            (substB e Γ)
-  | EMap l => EMap (map (fun '(x, y) => (substB x Γ, substB y Γ)) l)
-  | ETry e1 vl1 e2 vl2 e0 => ETry (substB e1 Γ)
-                                                vl1 (substB e2 Γ)
-                                                vl2 (substB e0 Γ)
+  | EValues el => match (sequence (map (fun x => subst_bigstep x Γ) el)) with
+                  | Some el' => Some (EValues el')
+                  | None => None
+                  end
+  | ENil => Some ENil
+  | ELit l => Some (ELit l)
+  | EVar v => match (get_value Γ (inl v)) with
+              | Some [v'] => value_to_expression v'
+              | _ => Some (EVar v)
+              end
+  | EFunId f =>  match (get_value Γ (inr f)) with
+                | Some [f'] => value_to_expression f'
+                | _ => Some (EFunId f)
+                end
+  | EFun vl e => match (subst_bigstep e Γ) with
+                | Some e' => Some (EFun vl e')
+                | None => None
+                end
+  | ECons hd tl => match (subst_bigstep hd Γ), (subst_bigstep tl Γ) with
+                  | Some hd', Some tl' => Some (ECons hd' tl')
+                  | _, _ => None
+                  end
+  | ETuple l => match (sequence (map (fun x => subst_bigstep x Γ) l)) with
+                | Some l' => Some (ETuple l')
+                | None => None
+                end
+  | ECall m f l => match (subst_bigstep m Γ), (subst_bigstep f Γ), (sequence (map (fun x => subst_bigstep x Γ) l)) with
+                  | Some m', Some f', Some l' => Some (ECall m' f' l')
+                  | _, _, _ => None
+                  end
+  | EPrimOp f l => match (sequence (map (fun x => subst_bigstep x Γ) l)) with
+                  | Some l' => Some (EPrimOp f l')
+                  | None => None
+                  end
+  | EApp exp l => match (subst_bigstep exp Γ), (sequence (map (fun x => subst_bigstep x Γ) l)) with
+                  | Some exp', Some l' => Some (EApp exp' l')
+                  | _, _ => None
+                  end
+  | ECase e l => match (subst_bigstep e Γ) with
+                | Some e' => match (sequence (map (fun '(pl, g, b) =>
+                                      match (subst_bigstep g Γ), (subst_bigstep b Γ) with
+                                      | Some g', Some b' => Some (pl, g', b')
+                                      | _, _ => None
+                                      end
+                                    ) l)) with
+                            | Some l' => Some (ECase e' l')
+                            | None => None
+                            end
+                | None => None
+                end
+  | ELet l e1 e2 => match (subst_bigstep e1 Γ), (subst_bigstep e2 Γ) with
+                    | Some e1', Some e2' => Some (ELet l e1' e2')
+                    | _, _ => None
+                    end
+  | ESeq e1 e2 => match (subst_bigstep e1 Γ), (subst_bigstep e2 Γ) with
+                  | Some e1', Some e2' => Some (ESeq e1' e2')
+                  | _, _ => None
+                  end
+  | ELetRec l e => match (sequence (map (fun '(fid, (vl, b)) =>
+                                      match (subst_bigstep b Γ) with
+                                      | Some b' => Some (fid, (vl, b'))
+                                      | None => None
+                                      end
+                                    ) l)), (subst_bigstep e Γ) with
+                    | Some l', Some e' => Some (ELetRec l' e')
+                    | _, _ => None
+                    end
+  | EMap l => match (sequence (map (fun '(x, y) => match (subst_bigstep x Γ), (subst_bigstep y Γ) with
+                                                  | Some x', Some y' => Some (x', y')
+                                                  | _, _ => None
+                                                  end
+                                                  ) l)) with
+              | Some l' => Some (EMap l')
+              | None => None
+              end
+  | ETry e1 vl1 e2 vl2 e0 => match (subst_bigstep e1 Γ), (subst_bigstep e2 Γ), (subst_bigstep e0 Γ) with
+                              | Some e1', Some e2', Some e0' => Some (ETry e1' vl1 e2' vl2 e0')
+                              | _, _, _ => None
+                              end
   end.
   
   
-  End SubstBigStep.
+  End subst_bigstepigStep.
