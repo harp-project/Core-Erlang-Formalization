832a6448a2b73f4680d7d9470778f5a78d3bbc3f
Peter Miskolczi <72017244+breadking99@users.noreply.github.com>
2024-05-23 11:45:40 +0200

    subst_env tests: fun in environment

--- src/TMP/Task/Equivalence.v
+++ src/TMP/Task/Equivalence.v
@@ -1437,6 +1437,130 @@ Section Test.
 
 
 
+  (* subst_env *)
+  
+  (*
+    env = []
+    x
+    x
+  *)
+  Lemma test_subst_env_1 : 
+    subst_env 10 
+              [] 
+              (EVar "x") 
+    = 
+    EVar "x".
+  Proof.
+    cbn. reflexivity.
+  Qed.
+
+
+
+  (*
+    env = [x = 1]
+    x
+    1
+  *)
+  Lemma test_subst_env_2 : 
+    subst_env 10 
+              [(inl "x" , VLit (Integer 1))] 
+              (EVar "x") 
+    = 
+    ELit (Integer 1).
+  Proof.
+    cbn. reflexivity.
+  Qed.
+
+
+
+  (*
+    env = [x = 1;
+           y = 2;
+           z = fun(x,y) -> x , y]
+    x , y , z
+    1 , 2 , fun(x,y) -> x , y
+  *)
+  Lemma test_subst_env_3 : 
+    subst_env 10 
+              [(inl "x" , VLit (Integer 1));
+               (inl "y" , VLit (Integer 2));
+               (inl "z" , VClos [] 
+                                [] 
+                                0 
+                                ["x"] 
+                                (EValues [EVar "x"; EVar "y"]) 
+                                None)]
+              (EValues [EVar "x"; EVar "y"; EVar "z"]) 
+    = 
+    (EValues [ELit (Integer 1); 
+              ELit (Integer 2); 
+              EFun ["x"] (EValues [EVar "x"; EVar "y"])]).
+  Proof.
+    cbn. reflexivity.
+  Qed.
+
+
+
+  (*
+    env = [x = 1;
+           y = 2;
+           z = fun(x) -> x , y]
+    x , y , app z [x]
+    1 , 2 , app (fun(x) -> x , y) [1]
+  *)
+  Lemma test_subst_env_4 : 
+    subst_env 10 
+              [(inl "x" , VLit (Integer 1));
+               (inl "y" , VLit (Integer 2));
+               (inl "z" , VClos [] 
+                                [] 
+                                0 
+                                ["x"] 
+                                (EValues [EVar "x"; EVar "y"]) 
+                                None)]
+              (EValues [EVar "x"; EVar "y"; EApp (EVar "z") [EVar "x"]]) 
+    = 
+    EValues [ELit (Integer 1); 
+             ELit (Integer 2); 
+             EApp (EFun ["x"] 
+                        (EValues [EVar "x"; EVar "y"])) 
+                  [ELit (Integer 1)]].
+  Proof.
+    cbn. reflexivity.
+  Qed.
+
+
+
+  (*
+    env = [x = 1;
+           y = 2;
+           z/1 = fun(x) -> x , y]
+    x , y , app z/1 [x]
+    1 , 2 , app (fun(x) -> x , y) [1]
+  *)
+  Lemma test_subst_env_5 : 
+    subst_env 10 
+              [(inl "x" , VLit (Integer 1));
+               (inl "y" , VLit (Integer 2));
+               (inr ("z" , 1) , VClos [] 
+                                      [] 
+                                      0 
+                                      ["x"] 
+                                      (EValues [EVar "x"; EVar "y"]) 
+                                      None)]
+              (EValues [EVar "x"; EVar "y"; EApp (EFunId ("z" , 1)) [EVar "x"]]) 
+    = 
+    EValues [ELit (Integer 1); 
+             ELit (Integer 2); 
+             EApp (EFun ["x"] 
+                        (EValues [EVar "x"; EVar "y"])) 
+                  [ELit (Integer 1)]].
+  Proof.
+    cbn. reflexivity.
+  Qed.
+
+  
+
 End Test.
 
 
