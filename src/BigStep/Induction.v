777b45ff38e4c9db60999253521e101f7ba15fed
Peter Miskolczi <72017244+breadking99@users.noreply.github.com>
2024-08-23 21:47:49 +0200

    measure_reduction_val mostly proved & derived_Expression_ind

--- src/BigStep/Induction.v
+++ src/BigStep/Induction.v
@@ -250,3 +250,39 @@ Proof.
 Defined.
 
 End derived_value_ind.
+
+Section derived_expression_ind.
+
+Variables
+  (P : Expression -> Prop).
+
+Hypotheses
+(HValues : forall el, Forall P el -> P (EValues el))
+(HNil : P ENil)
+(HLit : forall l, P (ELit l))
+(HVar : forall v, P (EVar v))
+(HFunId : forall f, P (EFunId f))
+(HFun : forall vl e, P e -> P (EFun vl e))
+(HCons : forall hd tl, P hd -> P tl -> P (ECons hd tl))
+(HTuple : forall l, Forall P l -> P (ETuple l))
+(HCall : forall m f l, P m -> P f -> Forall P l -> P (ECall m f l))
+(HPrimOp : forall f l, Forall P l -> P (EPrimOp f l))
+(HApp : forall exp l, P exp -> Forall P l -> P (EApp exp l))
+(HCase : forall e l, P e -> Forall (fun x => P (snd (fst x)) /\ P (snd x)) l -> P (ECase e l))
+(HLet : forall l e1 e2, P e1 -> P e2 -> P (ELet l e1 e2))
+(HSeq : forall e1 e2, P e1 -> P e2 -> P (ESeq e1 e2))
+(HLetRec : forall l e, P e -> Forall (fun x => P (snd (snd x))) l -> P (ELetRec l e))
+(HMap : forall l, Forall (fun x => P (fst x) /\ P (snd x)) l -> P (EMap l))
+(HTry : forall e1 vl1 e2 vl2 e3, P e1 -> P e2 -> P e3 -> P (ETry e1 vl1 e2 vl2 e3)).
+
+Theorem derived_Expression_ind :
+  forall e : Expression, P e.
+Proof.
+  induction e using Expression_ind2 with
+    (Q := Forall P)
+    (R := Forall (fun x => P (fst x) /\ P (snd x)))
+    (W := Forall (fun x => P (snd (fst x)) /\ P (snd x)))
+    (Z := Forall (fun x => P (snd (snd x)))); intuition.
+Defined.
+
+End derived_expression_ind.
--- src/Equivalence/BigStepToFrameStack/Simple/MeasureLemmas.v
+++ src/Equivalence/BigStepToFrameStack/Simple/MeasureLemmas.v
@@ -1,7 +1,10 @@
 From CoreErlang.Equivalence.BigStepToFrameStack.Simple Require Export Substitute.
 From CoreErlang.Equivalence.BigStepToFrameStack Require Export Measure.
 
+From CoreErlang Require Import Basics.
 Require Import stdpp.list.
+
+Import BigStep.
 
 (**
 * meausre_reduction [Admitted]
@@ -11,7 +14,21 @@ Require Import stdpp.list.
 
 
 
-Theorem measure_reduction :
+
+(*
+Theorem subst_env_empty :
+  forall n e,
+    subst_env n [] e = e.
+Proof.
+  intros.
+  destruct n.
+  { by cbn. }
+  induction e using derived_Expression_ind.
+  * 
+Admitted.
+*)
+
+Theorem measure_reduction_val :
   forall v n1 n2,
     measure_val v <= n1
   ->
@@ -22,62 +39,186 @@ Theorem measure_reduction :
     bval_to_bexp (subst_env n2) v.
 Proof.
   intros v n1 n2 Hn1 Hn2.
-  induction v.
+  induction v using derived_Value_ind.
   * by cbn.
   * by cbn.
-  * admit.
-  * cbn.
-    assert
-      (measure_val v1 ≤
-        measure_val (BigStep.Syntax.VCons v1 v2))
-      as Hv1.
-    {
-      cbn.
-      lia.
-    }
-    assert
-      (measure_val v2 ≤
-        measure_val (BigStep.Syntax.VCons v1 v2))
-      as Hv2.
-    {
-      cbn.
-      lia.
-    }
+  * (* Cons *)
+    remember
+      (BigStep.Syntax.VCons v1 v2)
+      as vcons
+      eqn:Heq_vcons.
+    assert (measure_val v1 ≤ measure_val vcons) as Hv1.
+    rewrite Heq_vcons; slia.
+    assert (measure_val v2 ≤ measure_val vcons) as Hv2.
+    rewrite Heq_vcons; slia.
     assert (measure_val v1 ≤ n1) as Hv1n1.
     {
-      apply Nat.le_trans
-        with (m := measure_val (BigStep.Syntax.VCons v1 v2)).
+      apply Nat.le_trans with (m := measure_val vcons).
       - exact Hv1.
       - exact Hn1.
     }
     assert (measure_val v1 ≤ n2) as Hv1n2.
     {
-      apply Nat.le_trans
-        with (m := measure_val (BigStep.Syntax.VCons v1 v2)).
+      apply Nat.le_trans with (m := measure_val vcons).
       - exact Hv1.
       - exact Hn2.
     }
     assert (measure_val v2 ≤ n1) as Hv2n1.
     {
-      apply Nat.le_trans
-        with (m := measure_val (BigStep.Syntax.VCons v1 v2)).
+      apply Nat.le_trans with (m := measure_val vcons).
       - exact Hv2.
       - exact Hn1.
     }
     assert (measure_val v2 ≤ n2) as Hv2n2.
     {
-      apply Nat.le_trans
-        with (m := measure_val (BigStep.Syntax.VCons v1 v2)).
+      apply Nat.le_trans with (m := measure_val vcons).
       - exact Hv2.
       - exact Hn2.
     }
+    clear Hv1 Hv2 Hn1 Hn2.
     specialize (IHv1 Hv1n1 Hv1n2).
     specialize (IHv2 Hv2n1 Hv2n2).
+    inv Heq_vcons.
+    clear H Hv1n1 Hv1n2 Hv2n1 Hv2n2.
+    cbn.
     rewrite IHv1.
     rewrite IHv2.
     reflexivity.
-  * admit.
-  * admit.
+  * (* Clos *)
+    induction ref as [| x env IHenv].
+    - cbn.
+      admit.
+    - admit.
+  * (* Tuple *)
+    induction l as [| v vl IHvl].
+    - by cbn.
+    - inv H.
+      rename H2 into Hv.
+      rename H3 into HForall.
+      remember
+        (BigStep.Syntax.VTuple vl)
+        as vtuple_vl
+        eqn:Heq_vtuple_vl.
+      remember
+        (BigStep.Syntax.VTuple (v :: vl))
+        as vtuple_vvl
+        eqn:Heq_vtuple_vvl.
+      assert (measure_val vtuple_vl ≤ measure_val vtuple_vvl) as Hvl.
+      {
+        rewrite Heq_vtuple_vl.
+        rewrite Heq_vtuple_vvl.
+        simpl.
+        unfold measure_list.
+        simpl.
+        slia.
+      }
+      assert (measure_val v ≤ n1) as Hvn1.
+      {
+        apply Nat.le_trans with (m := measure_val vtuple_vvl).
+        - inv Heq_vtuple_vvl. cbn. lia.
+        - exact Hn1.
+      }
+      assert (measure_val vtuple_vl ≤ n1) as Hvln1.
+      {
+        apply Nat.le_trans with (m := measure_val vtuple_vvl).
+        - exact Hvl.
+        - exact Hn1.
+      }
+      assert (measure_val v ≤ n2) as Hvn2.
+      {
+        apply Nat.le_trans with (m := measure_val vtuple_vvl).
+        - inv Heq_vtuple_vvl. cbn. lia.
+        - exact Hn2.
+      }
+      assert (measure_val vtuple_vl ≤ n2) as Hvln2.
+      {
+        apply Nat.le_trans with (m := measure_val vtuple_vvl).
+        - exact Hvl.
+        - exact Hn2.
+      }
+      clear Hn1 Hn2.
+      specialize (Hv Hvn1 Hvn2).
+      specialize (IHvl HForall Hvln1 Hvln2).
+      inv Heq_vtuple_vl.
+      clear H Hvn1 Hvln1 Hvn2 Hvln2 Hvl HForall.
+      simpl in *.
+      injection IHvl as Hmap_vl.
+      rewrite Hmap_vl.
+      rewrite Hv.
+      reflexivity.
+  * (* Map *)
+    induction l as [| (v1, v2) vl IHvl].
+    - by cbn.
+    - inv H.
+      rename H2 into Hv.
+      rename H3 into HForall.
+      simpl in Hv.
+      destruct Hv as [Hv1 Hv2].
+      remember
+        (BigStep.Syntax.VMap vl)
+        as vmap_vl
+        eqn:Heq_vmap_vl.
+      remember
+        (BigStep.Syntax.VMap ((v1, v2) :: vl))
+        as vmap_vvl
+        eqn:Heq_vmap_vvl.
+      assert (measure_val vmap_vl ≤ measure_val vmap_vvl) as Hvl.
+      {
+        rewrite Heq_vmap_vl.
+        rewrite Heq_vmap_vvl.
+        simpl.
+        unfold measure_list.
+        unfold measure_map.
+        slia.
+      }
+      assert (measure_val v1 ≤ n1) as Hv1n1.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - inv Heq_vmap_vvl. cbn. lia.
+        - exact Hn1.
+      }
+      assert (measure_val v2 ≤ n1) as Hv2n1.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - inv Heq_vmap_vvl. cbn. lia.
+        - exact Hn1.
+      }
+      assert (measure_val vmap_vl ≤ n1) as Hvln1.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - exact Hvl.
+        - exact Hn1.
+      }
+      assert (measure_val v1 ≤ n2) as Hv1n2.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - inv Heq_vmap_vvl. cbn. lia.
+        - exact Hn2.
+      }
+      assert (measure_val v2 ≤ n2) as Hv2n2.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - inv Heq_vmap_vvl. cbn. lia.
+        - exact Hn2.
+      }
+      assert (measure_val vmap_vl ≤ n2) as Hvln2.
+      {
+        apply Nat.le_trans with (m := measure_val vmap_vvl).
+        - exact Hvl.
+        - exact Hn2.
+      }
+      clear Hn1 Hn2.
+      specialize (Hv1 Hv1n1 Hv1n2).
+      specialize (Hv2 Hv2n1 Hv2n2).
+      specialize (IHvl HForall Hvln1 Hvln2).
+      inv Heq_vmap_vl.
+      clear H Hv1n1 Hv2n1 Hvln1 Hv1n2 Hv2n2 Hvln2 Hvl HForall.
+      simpl in *.
+      injection IHvl as Hvl.
+      rewrite Hv1.
+      rewrite Hv2.
+      rewrite Hvl.
+      reflexivity.
 Admitted.
 
 
@@ -85,7 +226,7 @@ Admitted.
 
 
 
-Theorem measure_reduction_list :
+Theorem measure_reduction_val_list :
   forall vl v1 v2,
     list_sum (map measure_val vl) <= measure_val v1
   ->
@@ -102,7 +243,7 @@ Admitted.
 
 
 
-Theorem measure_reduction_map :
+Theorem measure_reduction_val_map :
   forall ml v1 v2,
     list_sum (map (fun '(x, y) => (measure_val x) + (measure_val y)) ml)
       <= measure_val v1
@@ -121,5 +262,17 @@ Theorem measure_reduction_map :
         (bval_to_bexp (subst_env (measure_val v2)))
         (bval_to_bexp (subst_env (measure_val v2))))
       ml.
+Proof.
+Admitted.
+
+
+
+
+
+
+Theorem measure_reduction_subst_env :
+  forall env e n,
+    measure_env_exp env e <= n
+    -> subst_env n env e = subst_env (measure_env_exp env e) env e.
 Proof.
 Admitted.
