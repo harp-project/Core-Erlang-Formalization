0a0c2b96e96e544229a0d8702451d59b25fa944d
Arsenii Fomin <arseniy1304@gmail.com>
2025-04-08 00:41:15 +0200

    AtomExhaustionTests finalized

--- src/FrameStack/Vulnerabilities/AtomExhaustionExamples.v
+++ src/FrameStack/Vulnerabilities/AtomExhaustionExamples.v
@@ -1,8 +1,6 @@
 From CoreErlang.FrameStack Require Import SubstSemanticsLabeledLemmas.
 From stdpp Require Import gmap sets.
 Require Import AtomExhaustion.
-From Ltac2 Require Import Ltac2.
-From Ltac2 Require Option.
 Require Import List.
 
 Open Scope string_scope.
@@ -29,7 +27,7 @@ Ltac apply_proper_constr :=
 Goal generates_at_least_n_unique_atoms [] call_of_list_to_atom ∅ 1.
 Proof.
   unfold call_of_list_to_atom.
-  repeat ltac1:(apply_proper_constr).
+  repeat apply_proper_constr.
 Qed.
 
 Definition infinite_atom_g (e : Exp) : Exp :=
@@ -44,6 +42,20 @@ Definition infinite_atom_g (e : Exp) : Exp :=
 
 Ltac do_step :=
   eapply SubstSemanticsLabeled.step_trans; [econstructor;auto|simpl|simpl].
+
+Ltac do_n_do_step n :=
+lazymatch goal with
+| [ |- ⟨_ , _⟩ -[_, _]-> ⟨_ , _⟩] => 
+  match n with
+  | 0 => idtac "reached zero"
+  | S ?n' =>
+      eapply SubstSemanticsLabeled.step_trans;
+      [econstructor;(scope_solver || auto)
+      |simpl;do_n_do_step n'
+      |simpl;try auto;try cbn]
+  end
+| _ => idtac "failed to match"
+end.
 
 Goal atom_exhaustion (infinite_atom_g (˝VLit (Integer 0))) 20.
 Proof.
@@ -69,12 +81,11 @@ Restart.
   - eexists.
     (* This behavior obviously presents some problems for implementing 
        automatisation tactics. *)
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(scope_solver).
+    do_step. do_step. do_step.
     (* Third obstacle: as you may see in the environment tab, by using
-       step_trans constructor, the number of goals get inadvertently
+       step_trans constructor, the number of goals will get inadvertently
        bloated (due to the design of the step_trans constructor), which
        descreases readability and efficiency. *)
-    Fail do 2 ltac1:(do_step). (* and for some reason, I can't do this *)
 Restart.
   unfold atom_exhaustion, atom_exhaustion_aux.
   do 3 eexists. split.
@@ -86,7 +97,7 @@ Restart.
        when the number of the unique atoms in the resulting, but it is
        hard to reason about this, when one's only have lists, which may
        conatin duplicates. *)
-    + admit.
+    +
     (* Notice how we will only see ?l in the last goal. But what if we have
        100 applications of SubstSemanticsLabeledLemmas.transitive_eval? Will
        a proof engineer have to unfocus his current subgoal and scroll down
@@ -97,7 +108,7 @@ Theorem inf_atom_g: generates_at_least_n_unique_atoms []
   (infinite_atom_g (˝VLit (Integer 0))) ∅ 10.
 Proof.
   unfold infinite_atom_g.
-  repeat ltac1:(apply_proper_constr).
+  repeat apply_proper_constr.
   (* This, however, is much more elegant, albeit slow *)
 Qed.
 
@@ -120,90 +131,25 @@ Theorem sum_atom_g : exists e,
 Proof.
   exists (˝VLit (Integer 3)).
   unfold sum_example.
-  do 9 ltac1:(apply_proper_constr).
+  do 9 apply_proper_constr.
   (* Technical caveat of the tactic due to econstructor on match *)
   eapply generates_step_false.
   eapply SubstSemanticsLabeled.eval_step_case_not_match. reflexivity.
-  do 31 ltac1:(apply_proper_constr). ltac1:(replace (3 - 1)%Z with 2%Z by lia).
+  do 31 apply_proper_constr. replace (3 - 1)%Z with 2%Z by lia.
   eapply generates_step_false.
   eapply SubstSemanticsLabeled.eval_step_case_not_match. reflexivity.
-  do 31 ltac1:(apply_proper_constr). ltac1:(replace (2 - 1)%Z with 1%Z by lia).
+  do 31 apply_proper_constr. replace (2 - 1)%Z with 1%Z by lia.
   eapply generates_step_false.
   eapply SubstSemanticsLabeled.eval_step_case_not_match. reflexivity.
-  repeat ltac1:(apply_proper_constr).
+  repeat apply_proper_constr.
 Qed.
 
-Goal exists fs r l, ⟨[], infinite_atom_g (˝VLit 0%Z)⟩
-    -[l]->*
-  ⟨fs, r⟩.
-Proof.
-  do 4 eexists. unfold infinite_atom_g.
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(scope_solver).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(scope_solver).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-  ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-Abort.
-
-(* Ltac inf_atom_helper :=
+Ltac inf_atom_helper :=
 match goal with
-| |- generates_at_least_n_unique_atoms [FApp1 [˝ VLit (z + 1)%Z]]
-    _ avs (n - size _) =>
+| [|- context [?n - size (?s)]] =>
+    cbn; replace (size s) with 1 by set_solver; cbn; simpl
+end.
 
-  replace (mk_atom_set [(AtomCreation, [VLit
-    (String (ascii_of_nat (Z.to_nat 97))
-      (String (ascii_of_nat (Z.to_nat z)) ""))])]) with
-    (({[(String (ascii_of_nat (Z.to_nat 97))
-      (String (ascii_of_nat (Z.to_nat z)) ""))]}):gset string)
-    by set_solver;
-  replace (size {[String (ascii_of_nat (Z.to_nat 97))
-   (String (ascii_of_nat (Z.to_nat z)) "")]} ∖ avs) with 1 by set_solver *)
-(* end. *)
-(* generates_at_least_n_unique_atoms [FApp1 [˝ VLit (0 + 1)%Z]]
-  (˝ VClos
-       [(0, 1,
-         ° ELet 1 (˝ VCons (VLit 97%Z) (VCons (VVar 1) VNil))
-             (° ESeq
-                  (° ECall (˝ VLit "erlang") (˝ VLit "list_to_atom")
-                       [˝ VVar 0])
-                  (° ELet 1
-                       (° ECall (˝ VLit "erlang") 
-                            (˝ VLit "+") [˝ VVar 2; ˝ VLit 1%Z])
-                       (° EApp (˝ VFunId (2, 1)) [˝ VVar 0]))))] 0 1
-       (° ELet 1 (˝ VCons (VLit 97%Z) (VCons (VVar 1) VNil))
-            (° ESeq
-                 (° ECall (˝ VLit "erlang") (˝ VLit "list_to_atom")
-                      [˝ VVar 0])
-                 (° ELet 1
-                      (° ECall (˝ VLit "erlang") (˝ VLit "+")
-                           [˝ VVar 2; ˝ VLit 1%Z])
-                      (° EApp (˝ VFunId (2, 1)) [˝ VVar 0])))))
-  (∅
-   ∪ mk_atom_set
-       [(AtomCreation,
-         [VLit
-            (String (ascii_of_nat (Z.to_nat 97))
-               (String (ascii_of_nat (Z.to_nat 0)) ""))])])
-  (11 -
-   size
-     (mk_atom_set
-        [(AtomCreation,
-          [VLit
-             (String (ascii_of_nat (Z.to_nat 97))
-                (String (ascii_of_nat (Z.to_nat 0)) ""))])] ∖ ∅))
- *)
 Goal atom_exhaustion
   (infinite_atom_g (˝VLit (Integer 0))) 10.
 Proof.
@@ -223,36 +169,15 @@ Proof.
     -[[(AtomCreation, [VLit (String (ascii_of_nat (Z.to_nat 97))
        (String (ascii_of_nat (Z.to_nat z)) ""))])]]->*
       ⟨[FApp1 [˝ VLit (z + 1)%Z]], closRedex⟩) as Reach.
-  { subst. intros. eexists.
-    ltac1:(do_step). ltac1:(scope_solver).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). apply SubstSemanticsLabeled.step_refl. all:auto. }
+  { subst. intros. eexists. do_n_do_step 31.
+    apply SubstSemanticsLabeled.step_refl. }
   subst.
   (* ----------------------------------------------------------- *)
   apply soundness. simpl.
-  do 2 ltac1:(apply_proper_constr).
-  ltac1:(eapply galnua_multistep_rev_alt; [apply Reach|]).
-  ltac1:(replace (mk_atom_set [(AtomCreation, [VLit
-    (String (ascii_of_nat (Z.to_nat 97))
-      (String (ascii_of_nat (Z.to_nat 0)) ""))])]) with
-    (({[(String (ascii_of_nat (Z.to_nat 97))
-      (String (ascii_of_nat (Z.to_nat 0)) ""))]}):gset string)
-    by set_solver).
-  ltac1:(replace ({[String (ascii_of_nat (Z.to_nat 97))
-    (String (ascii_of_nat (Z.to_nat 0)) "")]} ∖ ∅) with
-    ({[String (ascii_of_nat (Z.to_nat 97))
-      (String (ascii_of_nat (Z.to_nat 0)) "")]}:gset string)
-    by set_solver).
-  ltac1:(replace (size {[String (ascii_of_nat (Z.to_nat 97))
-   (String (ascii_of_nat (Z.to_nat 0)) "")]}) with 1 by set_solver).
-  simpl.
-Admitted.
+  do 2 apply_proper_constr.
+  do 11 (eapply galnua_multistep_rev_alt;
+    [apply Reach|]; inf_atom_helper). apply_proper_constr.
+Qed.
 
 Fixpoint mk_sum_atom_list (n: nat) :=
 match n with
@@ -263,60 +188,39 @@ match n with
   :: mk_sum_atom_list n'
 end.
 
-Search Z "div".
-
 Theorem sum_eval (n: nat) :
   ⟨[], sum_example (˝VLit (Z.of_nat n))⟩
     -[mk_sum_atom_list n]->*
   ⟨[], RValSeq [VLit (Z.div (Z.of_nat ((n + 1) * n)) 2%Z)]⟩.
 Proof.
   unfold sum_example. induction n.
-  - eexists.
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(scope_solver).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). econstructor. all: auto.
-  - ltac1:(inv IHn). ltac1:(inv H). ltac1:(inv H0). simpl in H1.
-    ltac1:(inv H1). ltac1:(inv H). ltac1:(inv H0). ltac1:(inv H).
-    clear H4. ltac1:(inv H1). ltac1:(inv H). ltac1:(inv H0). ltac1:(inv H).
-    clear H10. ltac1:(inv H1). ltac1:(inv H). clear H4. ltac1:(inv H0).
-    ltac1:(inv H). simpl in H9. ltac1:(inv H9). eexists.
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(scope_solver).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). eapply SubstSemanticsLabeled.step_trans.
+  - eexists. do_n_do_step 13. econstructor.
+  - inv IHn. inv H. inv H0. simpl in H1. inv H1. inv H. inv H0. inv H.
+    clear H4. inv H1. inv H. inv H0. inv H. clear H10. inv H1. inv H.
+    clear H4. inv H0. inv H. simpl in H9. inv H9. eexists.
+    (* ---------------------------------------------------------- *)
+    do_n_do_step 9. eapply SubstSemanticsLabeled.step_trans.
     eapply SubstSemanticsLabeled.eval_step_case_not_match. reflexivity.
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(scope_solver).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step).
+    do_n_do_step 29.
     eapply SubstSemanticsLabeledLemmas.transitive_eval.
-    ltac1:(replace (Z.of_nat (S n) - 1)%Z with (Z.of_nat n)%Z by lia).
+    replace (Z.of_nat (S n) - 1)%Z with (Z.of_nat n)%Z by lia.
     { eapply (SubstSemanticsLabeledLemmas.frame_indep_nil _ _ _ _ _ H1). }
-    all: clear H1.
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). ltac1:(do_step).
-    ltac1:(do_step). ltac1:(do_step). ltac1:(do_step). unfold eval_arith.
-    simpl.
-    assert ((Z.of_nat (S n) + Z.of_nat ((n + 1) * n) `div` 2)%Z
-      = (Z.of_nat (S (n + (n + 1) * S n)) `div` 2)%Z).
-    { admit. } rewrite H. econstructor. all: auto.
-    ltac1:(replace (Z.to_nat 97) with 97 by lia).
-    ltac1:(replace (Z.to_nat (Z.of_nat (S n))) with (S n) by lia).
-    rewrite app_nil_r. reflexivity.
-Admitted.
+    all: clear H1. do_n_do_step 11. unfold eval_arith. simpl.
+    (* ---------------------------------------------------------- *)
+    + replace 2%Z with (Z.of_nat 2) by lia.
+      rewrite <- Nat2Z.inj_div, <- Nat2Z.inj_add, <- Nat2Z.inj_div.
+      replace (S (n+(n+1) * S n)) with ((S n * 2) + ((n+1)*n)) by lia.
+      rewrite Nat.div_add_l; auto. econstructor.
+    + replace (Z.to_nat 97) with 97 by lia.
+      replace (Z.to_nat (Z.of_nat (S n))) with (S n) by lia.
+      rewrite app_nil_r. reflexivity.
+Qed.
 
 Goal exists e, atom_exhaustion (sum_example e) 10.
 Proof.
   exists (˝VLit (Integer 11)). apply soundness; simpl.
-  ltac1:(pose proof sum_eval).
-  eapply galnua_multistep_rev_alt.
-  ltac1:(replace 11%Z with (Z.of_nat 11) by lia).
-  apply H. clear H.
+  pose proof sum_eval. eapply galnua_multistep_rev_alt.
+  replace 11%Z with (Z.of_nat 11) by lia. apply H. clear H.
   assert ((size ((mk_atom_set (mk_sum_atom_list 11)) ∖ ∅)) = 11).
-  { ltac1:(set_solver). } rewrite H. ltac1:(apply_proper_constr).
+  { set_solver. } rewrite H. apply_proper_constr.
 Qed.
