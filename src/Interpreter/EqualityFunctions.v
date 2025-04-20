4ce0bd5019c0b6f60b1af73910e85822d6e0ecfb
MTLEVR <mtlevr@inf.elte.hu>
2025-04-20 15:34:56 +0200

    Exp and Val strict equalities are reflexive

--- src/Interpreter/EqualityFunctions.v
+++ src/Interpreter/EqualityFunctions.v
@@ -106,3 +106,148 @@ with Val_eqb_strict (v1 v2 : Val) : bool :=
         end) ext ext'
   | _, _ => false
   end.
+
+Lemma Pat_eqb_refl: forall p, Pat_eqb p p = true.
+Proof.
+  induction p using Pat_ind2 with
+  (Q := Forall (fun p => Pat_eqb p p = true))
+  (R := Forall (PBoth (fun p => Pat_eqb p p = true))); simpl; auto.
+  * apply Lit_eqb_refl.
+  * now rewrite IHp1, IHp2.
+  * induction IHp; auto. now rewrite H, IHIHp.
+  * induction IHp; auto. destruct x, H. simpl in *. now rewrite H, H0, IHIHp.
+Qed.
+
+Lemma Exp_eqb_strict_refl: forall e, Exp_eqb_strict e e = true.
+Proof.
+  induction e using Exp_ind2 with
+    (PV := fun v => Val_eqb_strict v v = true)
+    (PE := fun e => Exp_eqb_strict e e = true)
+    (Q  := Forall (fun e => Exp_eqb_strict e e = true))
+    (QV := Forall (fun v => Val_eqb_strict v v = true))
+    (R  := Forall (PBoth (fun e => Exp_eqb_strict e e = true)))
+    (RV := Forall (PBoth (fun v => Val_eqb_strict v v = true)))
+    (VV := Forall (fun e => Exp_eqb_strict e.2 e.2 = true))
+    (W  := Forall (fun '(e1, e2, e3) => Exp_eqb_strict e3 e3 = true 
+                    /\ Exp_eqb_strict e2 e2 = true
+                    /\ Forall (fun p => Pat_eqb p p = true) e1))
+    (Z  := Forall (fun e => Exp_eqb_strict e.2 e.2 = true)); simpl; auto.
+  * apply Lit_eqb_refl.
+  * apply Nat.eqb_refl.
+  * rewrite IHe, IHe0. reflexivity.
+  * induction IHe; auto. rewrite H, IHIHe. reflexivity.
+  * induction IHe; auto. destruct x, H. simpl in *. rewrite H, H0, IHIHe. reflexivity.
+  * apply Nat.eqb_refl.
+  * destruct n. simpl. do 2 rewrite Nat.eqb_refl. reflexivity.
+  * rewrite IHe0, Nat.eqb_refl, Nat.eqb_refl. simpl.
+    clear -IHe. induction IHe; auto.
+    destruct x, p. simpl in H. rewrite IHIHe, H, Nat.eqb_refl, Nat.eqb_refl. reflexivity.
+  * now rewrite IHe, Nat.eqb_refl.
+  * induction IHe; auto. now rewrite H, IHIHe.
+  * now rewrite IHe1, IHe2.
+  * induction IHe; auto. now rewrite H, IHIHe.
+  * induction IHe; auto. destruct x, H. simpl in *. now rewrite H, H0, IHIHe.
+  * rewrite IHe1, IHe2. simpl.
+    clear -IHe3. induction IHe3; auto. now rewrite H, IHIHe3.
+  * rewrite String.eqb_refl. simpl. induction IHe; auto.
+    now rewrite H, IHIHe.
+  * rewrite IHe. simpl.
+    clear -IHe0. induction IHe0; auto. now rewrite H, IHIHe0.
+  * rewrite IHe, Nat.eqb_refl. simpl. induction IHe0; auto.
+    destruct x, p, H, H0. simpl in *. rewrite H, H0. simpl. rewrite IHIHe0.
+    clear -H1. induction H1; auto. rewrite H. simpl. rewrite IHForall. reflexivity.
+  * now rewrite IHe1, IHe2, Nat.eqb_refl.
+  * now rewrite IHe1, IHe2.
+  * rewrite IHe. induction IHe0; auto. destruct x. simpl in H.
+    rewrite Nat.eqb_refl. simpl in *. rewrite H. simpl. rewrite IHIHe0. reflexivity.
+  * now rewrite Nat.eqb_refl, Nat.eqb_refl, IHe1, IHe2, IHe3.
+  * constructor; auto.
+    split. auto. split. auto.
+    induction l; constructor. apply Pat_eqb_refl. apply IHl.
+Qed.
+
+Lemma Val_eqb_strict_refl: forall v, Val_eqb_strict v v = true.
+Proof.
+  induction v using Val_ind2 with
+    (P  := fun v => Exp_eqb_strict v v = true)
+    (PE := fun e => Exp_eqb_strict e e = true)
+    (Q  := Forall (fun e => Exp_eqb_strict e e = true))
+    (QV := Forall (fun v => Val_eqb_strict v v = true))
+    (R  := Forall (PBoth (fun e => Exp_eqb_strict e e = true)))
+    (RV := Forall (PBoth (fun v => Val_eqb_strict v v = true)))
+    (VV := Forall (fun e => Exp_eqb_strict e.2 e.2 = true))
+    (W  := Forall (fun '(e1, e2, e3) => Exp_eqb_strict e3 e3 = true 
+                    /\ Exp_eqb_strict e2 e2 = true
+                    /\ Forall (fun p => Pat_eqb p p = true) e1))
+    (Z  := Forall (fun e => Exp_eqb_strict e.2 e.2 = true)); simpl; auto.
+  * apply Lit_eqb_refl.
+  * apply Nat.eqb_refl.
+  * rewrite IHv1, IHv2. reflexivity.
+  * induction IHv; auto. rewrite H, IHIHv. reflexivity.
+  * induction IHv; auto. destruct x, H. simpl in *. rewrite H, H0, IHIHv. reflexivity.
+  * apply Nat.eqb_refl.
+  * destruct n. simpl. do 2 rewrite Nat.eqb_refl. reflexivity.
+  * rewrite IHv0, Nat.eqb_refl, Nat.eqb_refl. simpl.
+    clear -IHv. induction IHv; auto.
+    destruct x, p. simpl in H. rewrite IHIHv, H, Nat.eqb_refl, Nat.eqb_refl. reflexivity.
+  * now rewrite IHv, Nat.eqb_refl.
+  * induction IHv; auto. now rewrite H, IHIHv.
+  * now rewrite IHv, IHv0.
+  * induction IHv; auto. now rewrite H, IHIHv.
+  * induction IHv; auto. destruct x, H. simpl in *. now rewrite H, H0, IHIHv.
+  * rewrite IHv, IHv0. simpl.
+    clear -IHv1. induction IHv1; auto. now rewrite H, IHIHv1.
+  * rewrite String.eqb_refl. simpl. induction IHv; auto.
+    now rewrite H, IHIHv.
+  * rewrite IHv. simpl.
+    clear -IHv0. induction IHv0; auto. now rewrite H, IHIHv0.
+  * rewrite IHv, Nat.eqb_refl. simpl. induction IHv0; auto.
+    destruct x, p, H, H0. simpl in *. rewrite H, H0. simpl. rewrite IHIHv0.
+    clear -H1. induction H1; auto. rewrite H. simpl. rewrite IHForall. reflexivity.
+  * now rewrite IHv, IHv0, Nat.eqb_refl.
+  * now rewrite IHv, IHv0.
+  * rewrite IHv. induction IHv0; auto. destruct x. simpl in H.
+    rewrite Nat.eqb_refl. simpl in *. rewrite H. simpl. rewrite IHIHv0. reflexivity.
+  * now rewrite Nat.eqb_refl, Nat.eqb_refl, IHv, IHv0, IHv1.
+  * constructor; auto.
+    split. auto. split. auto.
+    induction l; constructor. apply Pat_eqb_refl. apply IHl.
+Qed.
+
+Lemma Val_eqb_strict_eq: forall v1 v2, Val_eqb_strict v1 v2 = true <-> v1 = v2.
+Proof.
+  intros. split.
+  * revert v1 v2. induction v1; intros.
+    + destruct v2; try discriminate. reflexivity.
+    + destruct v2; try discriminate. simpl in H. apply Lit_eqb_eq in H. rewrite H. reflexivity.
+    + destruct v2; try discriminate. simpl in H. apply Nat.eqb_eq in H. rewrite H. reflexivity.
+    + destruct v2; try discriminate. simpl in H. apply andb_true_iff in H. destruct H.
+      apply IHv1_1 in H. apply IHv1_2 in H0. rewrite H, H0. reflexivity.
+    + destruct v2; try discriminate. simpl in H. admit.
+    + destruct v2; try discriminate. simpl in H. admit.
+    + destruct v2; try discriminate. simpl in H. apply Nat.eqb_eq in H. rewrite H. reflexivity.
+    + destruct v2; try discriminate. simpl in H. unfold funid_eqb in H. destruct n, n0.
+      apply andb_true_iff in H. destruct H. apply Nat.eqb_eq in H, H0. rewrite H, H0. reflexivity.
+    + destruct v2; try discriminate. simpl in H. admit.
+  * intro. rewrite H. apply Val_eqb_strict_refl.
+Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
