98e3ee6ed5e3f6523c40897b06e381f00b67c66c
MTLEVR <mtlevr@inf.elte.hu>
2025-05-04 19:19:44 +0200

    functions for arrival actions and dead process actions

--- src/Interpreter/Scheduler.v
+++ src/Interpreter/Scheduler.v
@@ -1,4 +1,5 @@
 From CoreErlang.Interpreter Require Import StepFunctions.
+From CoreErlang.Concurrent  Require Import NodeSemantics.
 
 Print LiveProcess.
 Print FParams.
@@ -108,7 +109,57 @@ Definition nonArrivalAction : LiveProcess -> PID -> PID -> Action :=
       | _ => τ
     end.
 
-Print nonArrivalStep.
+Print nonArrivalAction.
+
+Definition arrivalActions : (* LiveProcess -> *) PID -> Ether -> list Action :=
+  fun destPID eth =>
+    let msgs := elements (dom eth)
+    in (fix f l := 
+      match l with
+        | [] => []
+        | (src, dst) :: l' => 
+          if dst =? destPID
+          then match eth !! (src, dst) with
+                 | None => f l' (* shouldn't be possible, since (src, dst) is in the domain *)
+                 | Some signals => ((fix g sl :=
+                                      match sl with
+                                        | [] => []
+                                        | s :: _ => [AArrive src dst s]
+                                      end
+                                   ) signals) ++ f l'
+               end
+          else f l'
+      end) msgs.
+
+Print arrivalActions.
+
+Compute arrivalActions 2 {[(1, 2) := [SLink]; (3, 4) := [SUnlink; SLink]]}.
+Compute arrivalActions 4 {[(1, 2) := [SLink]; (3, 4) := [SUnlink; SLink]]}.
+Compute arrivalActions 1 {[(1, 2) := [SLink]; (3, 4) := [SUnlink; SLink]]}.
+Compute arrivalActions 2 {[(1, 2) := [SLink]; (3, 2) := [SUnlink; SLink]]}.
+
+Definition deadActions : DeadProcess -> PID -> list Action :=
+  fun p selfPID =>
+    let links := elements (dom p)
+    in (fix f l :=
+      match l with
+        | [] => []
+        | linkPID :: l' => match p !! linkPID with
+                             | None => f l' (* shouldn't be possible, since linkPID is in the dom *)
+                             | Some reason => ASend selfPID linkPID (SExit reason true) :: f l'
+                           end
+      end
+    ) links.
+
+Compute deadActions {[2 := normal; 3 := kill; 4 := killed]} 1.
+
+Definition possibleActions : Process -> Ether -> PID -> PID -> list Action :=
+  fun p eth selfPID freshPID =>
+    match p with
+      | inl p' => nonArrivalAction p' selfPID freshPID :: arrivalActions selfPID eth
+      | inr p' => deadActions p' selfPID
+    end.
+
 
 (*
 func-actions(p, ether, config (selfpid, freshpid)) = list(actions)
