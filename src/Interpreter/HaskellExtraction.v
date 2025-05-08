7084c92dc538ead132e1b6ae227d51ea8953d176
MTLEVR <mtlevr@inf.elte.hu>
2025-05-08 11:22:40 +0200

    new haskell preprocessor

--- src/Interpreter/HaskellExtraction.v
+++ src/Interpreter/HaskellExtraction.v
@@ -3,17 +3,10 @@ Extraction Language Haskell.
 
 From CoreErlang.Interpreter Require Import StepFunctions.
 
+
 Require Import ExtrHaskellBasic.
 Require Import ExtrHaskellNatInteger.
 Require Import ExtrHaskellZInteger.
 Require Import ExtrHaskellString.
 
-Definition ex_set_1 : gset nat := {[1]}.
-Definition ex_set_2 : gset nat := {[2]}.
-Definition ex_set_3 : gset nat := {[1;2]}.
-
-Extraction "extr_test.hs" step_func processLocalStepFunc ex_set_1 ex_set_2 ex_set_3.
-
-Compute union ex_set_1 ex_set_2.
-Compute ex_set_1 ∪ ex_set_2.
-Compute ex_set_3.
+Extraction "extr_test.hs" step_func processLocalStepFunc interProcessStepFunc.
--- src/Interpreter/preprocess.sh
+++ src/Interpreter/preprocess.sh
@@ -4,33 +4,41 @@ file=$1
 
 temp=$(mktemp)
 
+import1="import qualified Data.Bits"
+import2="import qualified Data.Char"
+
+sed "6a\\
+$import1\\
+$import2" "$file" > "$temp"
+
 grep "^data" "$file" | \
 awk '{for (i=2; i<NF; i++) printf "%s ", $i; print ""}' | \
 awk '
-{
-	if (NF == 1) {
-		print "deriving instance Prelude.Show " $0
-		print "deriving instance GHC.Base.Eq " $0
-	} else {
-		printf "deriving instance ("
-		for (i=2; i<=NF; i++) {
-			printf "Prelude.Show %s", $i
-			if (i<NF) {
-				printf ", "
+{	
+	if ($1 != "Countable") {
+		if (NF == 1) {
+			print "deriving instance Prelude.Show " $0
+			print "deriving instance GHC.Base.Eq " $0
+		} else {
+			printf "deriving instance ("
+			for (i=2; i<=NF; i++) {
+				printf "Prelude.Show %s", $i
+				if (i<NF) {
+					printf ", "
+				}
 			}
-		}
-		print ") => Prelude.Show (" $0 ")"
+			print ") => Prelude.Show (" $0 ")"
 
-		printf "deriving instance ("
-		for (i=2; i<=NF; i++) {
-			printf "GHC.Base.Eq %s", $i
-			if (i<NF) {
-				printf ", "
+			printf "deriving instance ("
+			for (i=2; i<=NF; i++) {
+				printf "GHC.Base.Eq %s", $i
+				if (i<NF) {
+					printf ", "
+				}
 			}
+			print ") => GHC.Base.Eq (" $0 ")"
 		}
-		print ") => GHC.Base.Eq (" $0 ")"
 	}
-}' > "$temp"
+}' >> "$temp"
 
-cat "$temp" >> "$file"
-rm "$temp"
+mv "$temp" "$file"
