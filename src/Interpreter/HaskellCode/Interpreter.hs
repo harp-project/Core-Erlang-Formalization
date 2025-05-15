e617fa5ec60afad4e45dceaf7973aa35c32339c9
MTLEVR <mtlevr@inf.elte.hu>
2025-05-15 14:48:19 +0200

    New display for actions + better monad transformer syntax

--- src/Interpreter/HaskellCode/Interpreter.hs
+++ src/Interpreter/HaskellCode/Interpreter.hs
@@ -3,14 +3,29 @@ import Prelude
 import Control.Monad (when, unless, void)
 import Control.Monad.IO.Class
 import Control.Monad.State.Class
-import Control.Monad.Trans.State (runStateT)
+import Control.Monad.Trans.State (runStateT, StateT)
 
-type NodeState m = (MonadState (Node, RRConfig) m, MonadIO m)
+type NodeState = StateT (Node, RRConfig) IO
 
 exampleForExec :: (Node, RRConfig)
 exampleForExec = makeInitialNodeConf ex_Process
 
-evalKSteps :: NodeState m => Integer -> m ()
+displayAction :: PID -> Action -> NodeState ()
+displayAction pid action =
+  case action of
+    Coq__UU03c4_ -> return ()
+    Coq__UU03b5_ -> liftIO $ putStr "(P" >> putStr (show pid) >> putStrLn ") eps"
+    ASelf _ -> return ()
+    ASend ps pd sig -> do
+      liftIO $ putStr "(P" >> putStr (show ps) >> putStr ") ==[ (P" >> putStr (show pd) >> putStr ") ]==>\n\t"
+      liftIO $ print sig
+    AArrive ps pd sig -> do
+      liftIO $ putStr "(P" >> putStr (show pd) >> putStr ") <==[ (P" >> putStr (show ps) >> putStr ") ]==\n\t"
+      liftIO $ print sig
+    ASpawn p _ _ _ -> do
+      liftIO $ putStr "(P" >> putStr (show pid) >> putStr ") --{spawned}--> (P" >> putStr (show p) >> putStrLn ")"
+
+evalKSteps :: Integer -> NodeState ()
 evalKSteps 0 = finishOffIfDead
 evalKSteps k = do
   (node, conf) <- get
@@ -22,14 +37,13 @@ evalKSteps k = do
       else
         case nodeSimpleStep node (Left pid) of
           Just (node', action) -> do
-            liftIO $ putStr "PID #" >> print pid >> putStrLn "just make an action:"
-            liftIO $ print action
+            displayAction pid action
             put (node', newConfByAction conf action)
             evalKSteps (k-1)
           _ -> return ()
     _ -> return ()
 
-finishOffIfDead :: NodeState m => m ()
+finishOffIfDead :: NodeState ()
 finishOffIfDead =
   do
   (node, conf) <- get
@@ -40,11 +54,10 @@ finishOffIfDead =
         (if isTotallyDead node pid
         then
           void (put (node, delCurrFromConf conf))
-        else 
+        else
           (case nodeSimpleStep node (Left pid) of
             Just (node', action) -> do
-              liftIO $ putStr "PID #" >> print pid >> putStrLn "just make an action:"
-              liftIO $ print action
+              displayAction pid action
               put (node', conf)
               finishOffIfDead
             _ -> return ()))
