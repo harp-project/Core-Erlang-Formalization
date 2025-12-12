312aa835685ab242f6bd7d5f0b7494dce2d2a517
mtlevr <mtlevr@inf.elte.hu>
2025-12-12 12:35:19 +0100

    Organized symbolic evaluation into 3 new files, old files deleted.

---
+++ src/Symbolic/SymbExamples.v
@@ -0,0 +1,229 @@
+From CoreErlang.FrameStack Require Import SubstSemantics SubstSemanticsLemmas.
+From CoreErlang.Interpreter Require Import StepFunctions Equivalences.
+From CoreErlang.Symbolic Require Import SymbTheorems SymbTactics.
+
+Import ListNotations.
+
+Definition fact_frameStack (e : Exp) : Exp :=
+  ELetRec
+    [(1, °ECase (˝VVar 1) [
+      ([PLit 0%Z], ˝ttrue, (˝VLit 1%Z));
+      ([PVar], ˝ttrue,
+        °ELet 1 (EApp (˝VFunId (1, 1))
+          [°ECall (˝VLit "erlang"%string) (˝VLit "-"%string) [˝VVar 0; ˝VLit 1%Z]])
+          (°ECall (˝VLit "erlang"%string) (˝VLit "*"%string) [˝VVar 1; ˝VVar 0])
+      )
+    ])]
+    (EApp (˝VFunId (0, 1)) [e])
+   (* Write the definition here *)
+.
+
+Theorem fact_eval_ex:
+  forall (z : Z), (0 <= z)%Z ->
+  exists (y : Z),
+  ⟨ [], (fact_frameStack (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (z <= y /\ y >= 1)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition tailrec_fact (e d : Exp) : Exp :=
+  ELetRec [
+    (2, °ECase (˝VVar 1) [
+        ([PLit (Integer 0%Z)], ˝ttrue, ˝VVar 2);
+        ([PVar], ˝ttrue,
+          (°EApp (˝VFunId (1, 2)) 
+            [°ECall (˝erlang) (˝VLit "-"%string) [˝VVar 0; ˝VLit 1%Z];
+             °ECall (˝erlang) (˝VLit "*"%string) [˝VVar 0; ˝VVar 3]
+            ]))
+      ]
+    )
+  ] (EApp (˝VFunId (0, 2)) [e; d]) 
+.
+
+Theorem fact_tailrec_eval_ex:
+  forall (z : Z) (z' : Z), (0 <= z)%Z ->
+  exists (y : Z),
+  ⟨ [], (tailrec_fact (˝VLit z) (˝VLit z')) ⟩ -->* RValSeq [VLit y] /\ (y = Z.of_nat (Factorial.fact (Z.to_nat z)) * z')%Z.
+Proof.
+  solve_symbolically z z'.
+  
+  destruct PreCond0. subst.
+  rewrite Z.mul_assoc. f_equal.
+  rewrite <- positive_nat_Z.
+  rewrite <- Nat2Z.inj_mul. f_equal.
+  assert (1%Z = Z.of_nat (Z.to_nat 1%Z))%Z by lia. rewrite H0. clear H0.
+  rewrite <- Nat2Z.inj_sub;[|lia].
+  do 2 rewrite Nat2Z.id.
+  remember (Pos.to_nat p) as k.
+  pose proof Pos2Nat.is_pos p.
+  destruct k; try lia.
+  simpl.
+  rewrite Nat.sub_0_r. lia. 
+Qed.
+
+Definition timestwo (e : Exp) : Exp :=
+  ELetRec [
+      (1, °ECall (˝erlang) (˝VLit "*"%string) [˝VVar 1; ˝VLit 2%Z]
+      
+      )
+    ] (EApp (˝VFunId (0, 1)) [e]).
+
+Definition timestwo' (e : Exp) : Exp :=
+  °ECall (˝erlang) (˝VLit "*"%string) [e; ˝VLit 2%Z].
+
+Theorem timestwo_ex:
+  forall (z : Z), True ->
+  exists (y : Z),
+  ⟨ [], (timestwo (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Theorem timestwo'_ex:
+  forall (z : Z), True ->
+  exists (y : Z),
+  ⟨ [], (timestwo' (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition times_two_simple (e : Exp) : Exp :=
+  (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "*"%string))) [e;(VVal (VLit (Integer (2))))])).
+
+Theorem times_two_simple_ex:
+  forall (z : Z), True ->
+  exists (y : Z),
+  ⟨ [], (times_two_simple (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition times_two_rec (e : Exp) : Exp := ELetRec [
+(1, (EExp (ECase (VVal (VVar 1)) 
+[
+  ([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Integer (0)))));
+  ([PVar], (VVal (VLit (Atom "true"%string))), 
+    (EExp (ELet 1 (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "-"%string))) [(VVal (VVar 0));(VVal (VLit (Integer (1))))])) 
+    (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "+"%string))) [(EExp (EApp (VVal (VFunId (2, 1))) [(VVal (VVar 0))]));(VVal (VLit (Integer (2))))])))))])))] 
+
+(EApp (VVal (VFunId (0, 1))) [e]).
+
+Theorem times_two_rec_ex:
+  forall (z : Z), (0 <= z)%Z ->
+  exists (y : Z),
+  ⟨ [], (times_two_rec (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition plus_nums_simple (e f : Exp) : Exp :=
+(EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "+"%string))) [e;f])).
+
+Theorem plus_nums_simple_ex:
+  forall (z : Z) (z' : Z), True ->
+  exists (y : Z),
+  ⟨ [], (plus_nums_simple (˝VLit z) (˝VLit z')) ⟩ -->* RValSeq [VLit y] /\ (y = z + z')%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition plus_nums_rec (e f : Exp) := ELetRec [(2, (EExp (ECase (VVal (VVar 1)) [([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VVar 2)));([PVar], (VVal (VLit (Atom "true"%string))), (EExp (ELet 1 (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "-"%string))) [(VVal (VVar 0));(VVal (VLit (Integer (1))))])) (EExp (ELet 1 (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "+"%string))) [(VVal (VVar 4));(VVal (VLit (Integer (1))))])) (EExp (EApp (VVal (VFunId (3, 2))) [(VVal (VVar 1));(VVal (VVar 0))])))))))])))] (EApp (VVal (VFunId (0, 2))) [e;f]).
+
+Theorem plus_nums_rec_ex:
+  forall (z : Z),
+  exists (y : Z),
+  ⟨ [], (plus_nums_rec (˝VLit z) (˝VLit 0%Z)) ⟩ -->* RValSeq [VLit y] /\ (y = z)%Z.
+Proof.
+  (* This cannot be proven by induction, since the goal is too specific. *)
+Abort.
+
+Theorem plus_nums_rec_ex':
+  forall (z : Z) (z' : Z), (z >= 0)%Z ->
+  exists (y : Z),
+  ⟨ [], (plus_nums_rec (˝VLit z) (˝VLit z')) ⟩ -->* RValSeq [VLit y] /\ (y = z + z')%Z.
+Proof.
+  solve_symbolically z z'.
+Qed.
+
+
+Definition isitzero_atom (e : Exp) : Exp :=
+(EExp (ECase (e) [([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Atom "true"%string))));([PVar], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Atom "false"%string))))])).
+
+Theorem isitzero_atom_ex:
+  forall (z : Z), (z >= 0)%Z ->
+  exists (y : string),
+  ⟨ [], (isitzero_atom (˝VLit (Z.succ z))) ⟩ -->* RValSeq [VLit y] /\ (y = "false"%string)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition isitzero_num (e : Exp) : Exp :=
+(EExp (ECase (e) [([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Integer (1)))));([PVar], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Integer (0)))))])).
+
+Theorem isitzero_num_ex:
+  forall (z : Z), True ->
+  exists (y : Z),
+  ⟨ [], (isitzero_num (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ ((y = 0)%Z \/ (y = 1)%Z).
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition isitzero_num_app (e : Exp) : Exp :=
+EExp ( EApp ( EFun 1 (EExp (ECase (VVal (VVar 0)) [([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Integer (1)))));([PVar], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Integer (0)))))]))) [e]).
+
+Theorem isitzero_num_app_ex:
+  forall (z : Z), True ->
+  exists (y : Z),
+  ⟨ [], (isitzero_num_app (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ ((y = 0)%Z \/ (y = 1)%Z).
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition isitzero_atom_app (e : Exp) : Exp :=
+EExp ( EApp ( EFun 1(EExp (ECase (VVal (VVar 0)) [([(PLit (Integer (0)))], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Atom "true"%string))));([PVar], (VVal (VLit (Atom "true"%string))), (VVal (VLit (Atom "false"%string))))]))) [e]).
+
+Theorem isitzero_atom_app_ex:
+  forall (z : Z), (z > 0)%Z ->
+  exists (y : string),
+  ⟨ [], (isitzero_atom_app (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = "false"%string).
+Proof.
+  solve_symbolically z.
+Qed.
+
+Theorem timestwo_ex':
+  forall (z : Z),
+  exists (y : Z),
+  ⟨ [], (times_two_simple (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Definition times_two_simple_app (e : Exp) : Exp :=
+  EExp (EApp (EExp (EFun 1 (EExp (ECall (VVal (VLit (Atom "erlang"%string))) (VVal (VLit (Atom "*"%string))) [(VVal (VVar 0));(VVal (VLit (Integer (2))))])))) [e]).
+
+Theorem timestwo_ex'':
+  forall (z : Z),
+  exists (y : Z),
+  ⟨ [], (times_two_simple_app (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+Theorem timestwo_ex''':
+  forall (z : Z), (0 <= z)%Z ->
+  exists (y : Z),
+  ⟨ [], (times_two_rec (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (y = z * 2)%Z.
+Proof.
+  solve_symbolically z.
+Qed.
+
+
+
+
+
+
+
+
+
+
+
--- src/Symbolic/SymbTactics.v
+++ src/Symbolic/SymbTactics.v
@@ -1,393 +1,9 @@
 From CoreErlang.FrameStack Require Import SubstSemantics SubstSemanticsLemmas.
 From CoreErlang.Interpreter Require Import StepFunctions Equivalences.
+From CoreErlang.Symbolic Require Import SymbTheorems.
 
 Import ListNotations.
 
-(* ----- DEFINITIONS ----- *)
-
-Fixpoint sequentialStepMaxK (fs : FrameStack) (r : Redex) (n : nat) : FrameStack * Redex :=
-  match sequentialStepFunc fs r with
-  | None => (fs, r)
-  | Some (fs', r') =>
-    match n with
-    | 0 => (fs, r)
-    | S n' => sequentialStepMaxK fs' r' n'
-    end
-  end.
-
-Fixpoint sequentialStepK (fs : FrameStack) (r : Redex) (n : nat) : option (FrameStack * Redex) :=
-  match n with
-  | 0 => Some (fs, r)
-  | S n' =>
-    match sequentialStepFunc fs r with
-    | Some (fs', r') => sequentialStepK fs' r' n'
-    | None => None
-    end
-  end.
-
-Definition canRec (fs : FrameStack) (r : Redex) : bool :=
-  match fs with
-  | FParams (IApp (VClos (_ :: _) _ _ _)) _ [] :: _ => 
-    match r with
-    | RValSeq _ => true
-    | RBox => true
-    | _ => false
-    end
-  | _ => false
-  end.
-
-Definition lastParamRBox (fs : FrameStack) (r : Redex) : (FrameStack * Redex) :=
-  match fs, r with
-  | FParams ident vl ex :: fs', RValSeq [v] => (FParams ident (vl ++ [v]) ex :: fs', RBox)
-  | FParams ident vl ex :: fs', RBox => (FParams ident vl ex :: fs', RBox)
-  | fs', r => (fs', r)
-  end.
-
-Fixpoint sequentialStepCanRec (fs : FrameStack) (r : Redex) (n : nat) : FrameStack * Redex :=
-  match canRec fs r with
-  | true => lastParamRBox fs r
-  | false =>
-    match sequentialStepFunc fs r with
-    | None => (fs, r)
-    | Some (fs', r') =>
-      match n with
-      | 0 => (fs, r)
-      | S n' => sequentialStepCanRec fs' r' n'
-      end
-    end
-  end.
-
-Arguments sequentialStepMaxK !_ !_ !_ /.
-Arguments sequentialStepK !_ !_ !_ /.
-Arguments sequentialStepCanRec !_ !_ !_ /.
-
-(* ----- LEMMAS ----- *)
-
-Lemma maxKZeroRefl:
-  forall (fs : FrameStack) (r : Redex),
-  sequentialStepMaxK fs r 0 = (fs, r).
-Proof.
-  intros. unfold sequentialStepMaxK.
-  destruct (sequentialStepFunc fs r).
-  1:destruct p. all:reflexivity.
-Qed.
-
-Lemma maxKForwardOne:
-  forall (fs : FrameStack) (r r' : Redex),
-  is_result r' ->
-  (exists n, sequentialStepMaxK fs r (S n) = ([], r')) <->
-  exists n, sequentialStepMaxK fs r n = ([], r').
-Proof.
-  intros. split; intro.
-  * destruct H0. exists (S x). auto.
-  * destruct H0. destruct x.
-    + exists 0.
-      rewrite maxKZeroRefl in H0. inv H0.
-      inv H. all:auto.
-    + exists x. auto.
-Qed.
-
-Lemma maxKOverflow:
-  forall (fs : FrameStack) (r r' : Redex) (n m : nat),
-  is_result r' ->
-  n <= m ->
-  sequentialStepMaxK fs r n = ([], r') ->
-  sequentialStepMaxK fs r m = ([], r').
-Proof.
-  intros fs r r' n. revert fs r r'.
-  induction n; intros.
-  * destruct m.
-    + auto.
-    + rewrite maxKZeroRefl in H1. inv H1.
-      inv H. all:auto.
-  * destruct m.
-    + inv H0.
-    + unfold sequentialStepMaxK in H1|-*.
-      destruct (sequentialStepFunc fs r).
-      1:destruct p; fold sequentialStepMaxK.
-      all:fold sequentialStepMaxK in H1.
-      - apply IHn; auto. lia.
-      - auto.
-Qed.
-
-Lemma maxKForwardThousand:
-  forall (fs : FrameStack) (r r' : Redex),
-  is_result r' ->
-  (exists n, sequentialStepMaxK fs r (1000 + n) = ([], r')) <->
-  exists n, sequentialStepMaxK fs r n = ([], r').
-Proof.
-  intros. split; intro.
-  * destruct H0. exists (1000 + x). auto.
-  * destruct H0. 
-    exists x.
-    apply (maxKOverflow _ _ _ x); auto. lia.
-Qed.
-
-Lemma maxKEquivK:
-  forall (fs fs' : FrameStack) (r r' : Redex),
-  (exists n, sequentialStepK fs r n = Some (fs', r')) <->
-  (exists n, sequentialStepMaxK fs r n = (fs', r')).
-Proof.
-  intros. split;intro.
-  * destruct H. exists x.
-    revert H. revert fs r.
-    induction x; intros.
-    + unfold sequentialStepK in *. inv H.
-      rewrite maxKZeroRefl. auto.
-    + unfold sequentialStepMaxK.
-      unfold sequentialStepK in H.
-      destruct (sequentialStepFunc fs r).
-      1:destruct p.
-      all:fold sequentialStepMaxK. all:fold sequentialStepK in H.
-      all:auto. inv H.
-  * destruct H. revert H. revert fs r.
-    induction x; intros.
-    + rewrite maxKZeroRefl in H. inv H. exists 0.
-      unfold sequentialStepK. reflexivity.
-    + unfold sequentialStepMaxK in H.
-      destruct (sequentialStepFunc fs r) eqn:Hssf; fold sequentialStepMaxK in H.
-      - destruct p.
-        apply IHx in H. destruct H. exists (S x0).
-        unfold sequentialStepK. rewrite Hssf. fold sequentialStepK.
-        auto.
-      - inv H. exists 0. unfold sequentialStepK. reflexivity.
-Qed.
-
-Lemma kEquiv:
-  forall (fs fs' : FrameStack) (r r' : Redex) (k : nat),
-  ⟨ fs, r ⟩ -[k]-> ⟨ fs', r' ⟩ <-> sequentialStepK fs r k = Some (fs', r').
-Proof.
-  intros. split;revert fs fs' r r'.
-  * induction k; intros.
-    + inv H. unfold sequentialStepK. auto.
-    + inv H. unfold sequentialStepK.
-      apply sequentialStepEquiv in H1. rewrite H1.
-      fold sequentialStepK. auto.
-  * induction k; intros.
-    + unfold sequentialStepK in H. inv H. constructor.
-    + unfold sequentialStepK in H.
-      destruct (sequentialStepFunc fs r) eqn:Hssf.
-      - destruct p. fold sequentialStepK in H.
-        apply sequentialStepEquiv in Hssf.
-        econstructor; eauto.
-      - inv H.
-Qed.
-
-Theorem RTCEquiv:
-  forall (fs : FrameStack) (r r' : Redex),
-  is_result r' ->
-  ⟨ fs, r ⟩ -->* r' <-> exists n, sequentialStepMaxK fs r n = ([], r').
-Proof.
-  intros fs r r' Hres. split; intros.
-  * inv H. destruct H0.
-    apply kEquiv in H0.
-    apply maxKEquivK.
-    exists x. auto.
-  * apply maxKEquivK in H.
-    destruct H. econstructor. split;[auto|].
-    apply kEquiv. eauto.
-Qed.
-
-Lemma canRecUnfold:
-  forall (fs : FrameStack) (r : Redex),
-  canRec fs r = true -> 
-    exists ext_top ext' id params e vl fs',
-    (fs = FParams (IApp (VClos (ext_top :: ext') id params e)) vl [] :: fs') /\ 
-    ((exists vseq, r = RValSeq vseq) \/ (r = RBox)).
-Proof.
-  intros. unfold canRec in H.
-  destruct fs; try discriminate. destruct f; try discriminate. destruct ident; try discriminate.
-  destruct v; try discriminate. destruct ext; try discriminate. destruct el; try discriminate.
-  destruct r; try discriminate.
-  * do 8 eexists. 1:reflexivity. left. eexists. reflexivity.
-  * do 8 eexists. 1:reflexivity. right. reflexivity.
-Qed.
-
-Lemma maxKTransCanRec:
-  forall (fs fs': FrameStack) (r r' r'': Redex) (k : nat),
-  is_result r'' ->
-  sequentialStepCanRec fs r k = (fs', r') ->
-  (exists n, sequentialStepMaxK fs' r' n = ([], r'')) ->
-  (exists n, sequentialStepMaxK fs r n = ([], r'')).
-Proof.
-  intros.
-  destruct H1. revert H0 H1. revert x fs fs' r r'.
-  induction k; intros.
-  + unfold sequentialStepCanRec in H0.
-    destruct (canRec fs r) eqn:HCanRec.
-    - apply canRecUnfold in HCanRec. 
-      destruct HCanRec as [ext_top [ext' [id [params [e [vl [fs'' [Hcr1 [[vs Hcr2] | Hcr2]]]]]]]]].
-      * subst. simpl in H0. destruct vs.
-        ++ inv H0. exists x. auto.
-        ++ destruct vs.
-           -- inv H0. destruct x.
-              ** rewrite maxKZeroRefl in H1. inv H1.
-              ** exists (S x).
-                 simpl in H1. simpl.
-                 destruct (params =? length (vl ++ [v]));auto.
-           -- inv H0. exists x. auto.
-      * subst. simpl in H0. inv H0. exists x. auto.
-    - destruct (sequentialStepFunc fs r) eqn:Hssf. 1:destruct p. all:inv H0. all:eexists;eauto.
-  + simpl in H0.
-    destruct (canRec fs r) eqn:HCanRec.
-    - apply canRecUnfold in HCanRec.
-      destruct HCanRec as [ext_top [ext' [id [params [e [vl [fs'' [Hcr1 [[vs Hcr2] | Hcr2]]]]]]]]].
-      * subst. simpl in H0. destruct vs.
-        ++ inv H0. exists x. auto.
-        ++ destruct vs.
-           -- inv H0. destruct x.
-               ** rewrite maxKZeroRefl in H1. inv H1.
-               ** exists (S x).
-                  simpl in H1. simpl.
-                  destruct (params =? length (vl ++ [v]));auto.
-            -- inv H0. exists x. auto.
-      * subst. simpl in H0. inv H0. exists x. auto.
-    - destruct (sequentialStepFunc fs r) eqn:Hssf.
-      ** destruct p.
-         unfold sequentialStepCanRec in H0. rewrite HCanRec in H0. rewrite Hssf in H0.
-         fold sequentialStepCanRec in H0.
-         specialize (IHk _ _ _ _ _ H0 H1).
-         destruct IHk. exists (S x0). unfold sequentialStepMaxK. rewrite Hssf.
-         fold sequentialStepMaxK. auto.
-      ** inv H0.
-         destruct x.
-         ++ rewrite maxKZeroRefl in H1. inv H1. exists 0.
-            unfold sequentialStepCanRec in H3. rewrite HCanRec in H3. rewrite Hssf in H3. inv H3.
-            apply maxKZeroRefl.
-         ++ unfold sequentialStepMaxK in H1.
-            unfold sequentialStepCanRec in H3.
-            rewrite HCanRec in H3. rewrite Hssf in H3.  inv H3.
-            rewrite Hssf in H1. inv H1.
-            exists 0. apply maxKZeroRefl.
-Qed.
-
-Lemma maxKInsertCanRecGeneral:
-  forall (fs : FrameStack) (r r'' : Redex) (k : nat),
-  is_result r'' ->
-  (exists n, (let (fs', r') := sequentialStepCanRec fs r k in sequentialStepMaxK fs' r' n) = ([], r'')) <->
-  (exists n, sequentialStepMaxK fs r n = ([], r'')).
-Proof.
-  intros. split; intros.
-  * destruct (sequentialStepCanRec fs r k) eqn:Hsscr.
-    eapply maxKTransCanRec; eauto.
-  * destruct H0. 
-    destruct (sequentialStepCanRec fs r k) eqn:Hsscr.
-    revert H0 Hsscr. revert fs r f r0 k.
-    induction x; intros.
-    + rewrite maxKZeroRefl in H0. inv H0. inv H.
-      all:destruct k.
-      all:inv Hsscr. all:exists 0; auto.
-    + unfold sequentialStepMaxK in H0.
-      destruct (sequentialStepFunc fs r) eqn:Hssf.
-      - destruct p. fold sequentialStepMaxK in H0.
-        destruct k.
-        ** simpl in Hsscr.
-           destruct (canRec fs r) eqn:HCanRec.
-           ++ apply canRecUnfold in HCanRec.
-              destruct HCanRec as [ext_top [ext' [id [params [e [vl [fs'' [Hcr1 [[vs Hcr2] | Hcr2]]]]]]]]].
-              -- subst. simpl in Hsscr. destruct vs.
-                 *** inv Hsscr.
-                 *** destruct vs.
-                     +++ inv Hsscr.
-                         exists (S x). simpl.
-                         simpl in Hssf.
-                         destruct (params =? length (vl ++ [v])); inv Hssf; auto.
-                     +++ inv Hsscr.
-              -- subst. simpl in Hsscr. inv Hsscr.
-                 exists (S x). unfold sequentialStepMaxK.
-                 rewrite Hssf. fold sequentialStepMaxK. auto.
-           ++ unfold sequentialStepCanRec in Hsscr. rewrite HCanRec in Hsscr.
-              rewrite Hssf in Hsscr. inv Hsscr. exists (S x).
-              unfold sequentialStepMaxK. rewrite Hssf. fold sequentialStepMaxK. auto.
-        ** simpl in Hsscr.
-           destruct (canRec fs r) eqn:HCanRec.
-           ++ apply canRecUnfold in HCanRec.
-              destruct HCanRec as [ext_top [ext' [id [params [e [vl [fs'' [Hcr1 [[vs Hcr2] | Hcr2]]]]]]]]].
-              -- subst. simpl in Hsscr. destruct vs.
-                 *** inv Hsscr.
-                 *** destruct vs.
-                     +++ inv Hsscr.
-                         exists (S x). unfold sequentialStepMaxK. simpl. simpl in Hssf.
-                         rewrite Hssf. fold sequentialStepMaxK. auto.
-                     +++ inv Hsscr.
-              -- subst. simpl in Hsscr. inv Hsscr. exists (S x).
-                 unfold sequentialStepMaxK. rewrite Hssf. fold sequentialStepMaxK. auto.
-           ++ unfold sequentialStepCanRec in Hsscr. rewrite HCanRec in Hsscr.
-              rewrite Hssf in Hsscr.
-              specialize (IHx _ _ _ _ _ H0 Hsscr). auto.
-      - inv H0.
-        destruct k.
-        ** unfold sequentialStepCanRec in Hsscr.
-           destruct (canRec [] r'') eqn:HCanRec.
-           ++ simpl in HCanRec. discriminate.
-           ++ rewrite Hssf in Hsscr. inv Hsscr. exists 0. apply maxKZeroRefl.
-        ** unfold sequentialStepCanRec in Hsscr. inv H; simpl in Hsscr; inv Hsscr.
-           all:exists 0; auto.
-Qed.
-
-Lemma maxKInsertCanRec:
-  forall (fs : FrameStack) (r r'' : Redex),
-  is_result r'' ->
-  (exists n, (let (fs', r') := sequentialStepCanRec fs r 1000 in sequentialStepMaxK fs' r' n) = ([], r'')) <->
-  (exists n, sequentialStepMaxK fs r n = ([], r'')).
-Proof.
-  intros fs r r''.
-  exact (maxKInsertCanRecGeneral fs r r'' 1000).
-Qed.
-
-Theorem frame_indep_core_func:
-  forall (fs fs' : FrameStack) (r r' : Redex),
-  (exists n, sequentialStepMaxK fs r n = (fs', r')) ->
-  forall (fsapp : FrameStack), (exists n, sequentialStepMaxK (fs ++ fsapp) r n = (fs' ++ fsapp, r')).
-Proof.
-  intros.
-  apply maxKEquivK. apply maxKEquivK in H.
-  destruct H. apply kEquiv in H.
-  exists x. apply kEquiv. apply frame_indep_core. auto.
-Qed.
-
-Theorem maxKTransitive:
-  forall (fs fs' fs'' : FrameStack) (r r' r'' : Redex),
-  (exists n, sequentialStepMaxK fs r n = (fs', r')) ->
-  (exists n, sequentialStepMaxK fs' r' n = (fs'', r'')) ->
-  (exists n, sequentialStepMaxK fs r n = (fs'', r'')).
-Proof.
-  setoid_rewrite <- maxKEquivK. setoid_rewrite <- kEquiv. intros.
-  destruct H, H0. exists (x + x0).
-  eapply transitive_eval; eauto.
-Qed.
-
-Lemma maxKDone:
-  forall (r r' : Redex),
-  is_result r' ->
-  (exists n : nat, ([] : FrameStack, r') = ([], r)) <->
-  (exists n, sequentialStepMaxK [] r' n = ([], r)).
-Proof.
-  intros. split;intro.
-  * destruct H0. inv H0. setoid_rewrite <- RTCEquiv;[|auto].
-    econstructor. split. auto. constructor.
-  * destruct H0. destruct x.
-    + rewrite maxKZeroRefl in H0. exists 0. auto.
-    + inv H; simpl in H0; exists 0; auto.
-Qed.
-
-Lemma Z_eqb_eq_corr : forall (z1 z2 : Z), (z1 =? z2)%Z = true -> z1 = z2. Proof. lia. Qed.
-Lemma Z_eqb_neq_corr: forall (z1 z2 : Z), (z1 =? z2)%Z = false-> z1 <>z2. Proof. lia. Qed.
-
-Theorem maxKTransitive':
-  forall (fs fs' fs'' : FrameStack) (r r' r'' : Redex) (P : Prop),
-  (exists n, sequentialStepMaxK fs r n = (fs', r')) ->
-  ((exists n, sequentialStepMaxK fs' r' n = (fs'', r'')) /\ P) ->
-  ((exists n, sequentialStepMaxK fs r n = (fs'', r'')) /\ P).
-Proof.
-  setoid_rewrite <- maxKEquivK. setoid_rewrite <- kEquiv. intros.
-  destruct H, H0, H0.
-  split;auto.
-  exists (x + x0).
-  eapply transitive_eval; eauto.
-Qed.
-
-(* ----- TACTICS ----- *)
 
 Ltac contains_match :=
   lazymatch goal with
@@ -480,6 +96,7 @@ Ltac solve_final_state :=
 
 Ltac solve_final_postcond :=
   first [ nia
+        | auto
         | idtac "Could not solve postcondition"
         ].
 
@@ -719,7 +336,15 @@ Ltac take_to_rec_loop_mult h t :=
           separate_cases_mult h t;
           solve_symbolically_internal_mult h t
         | (* If we did not hit a point of recursion, or a case separation,
-             the loop needs to be continued. *)
+             the loop needs to be continued. 
+             
+             A single step is done manually, 
+             because non-recursive functions defined in a LetRec can cause issues:
+             we can get to a point of potential recursion, but since the function is
+             not in fact recursive, that branch will fail. Without this stepOne, we
+             can run into an infinite loop.
+             *)
+          stepOne;
           solve_symbolically_internal_mult h t
         ]
 with
@@ -903,7 +528,15 @@ Ltac take_to_rec_loop_0 h :=
           separate_cases_0 h;
           solve_symbolically_internal_0 h
         | (* If we did not hit a point of recursion, or a case separation,
-             the loop needs to be continued. *)
+             the loop needs to be continued. 
+             
+             A single step is done manually, 
+             because non-recursive functions defined in a LetRec can cause issues:
+             we can get to a point of potential recursion, but since the function is
+             not in fact recursive, that branch will fail. Without this stepOne, we
+             can run into an infinite loop.
+             *)
+          stepOne;
           solve_symbolically_internal_0 h
         ]
 with
@@ -930,75 +563,3 @@ Tactic Notation "solve_symbolically" ident(h) :=
        Am I right, is it only possible to have a function application with an empty list of
        parameters left to evaluate on the *top* of the frame stack?
 *)
-
-(* ----- EXAMPLES ----- *)
-
-Definition fact_frameStack (e : Exp) : Exp :=
-  ELetRec
-    [(1, °ECase (˝VVar 1) [
-      ([PLit 0%Z], ˝ttrue, (˝VLit 1%Z));
-      ([PVar], ˝ttrue,
-        °ELet 1 (EApp (˝VFunId (1, 1))
-          [°ECall (˝VLit "erlang"%string) (˝VLit "-"%string) [˝VVar 0; ˝VLit 1%Z]])
-          (°ECall (˝VLit "erlang"%string) (˝VLit "*"%string) [˝VVar 1; ˝VVar 0])
-      )
-    ])]
-    (EApp (˝VFunId (0, 1)) [e])
-   (* Write the definition here *)
-.
-
-Theorem fact_eval_ex:
-  forall (z : Z), (0 <= z)%Z ->
-  exists (y : Z),
-  ⟨ [], (fact_frameStack (˝VLit z)) ⟩ -->* RValSeq [VLit y] /\ (z <= y /\ y >= 1)%Z.
-Proof.
-  solve_symbolically z.
-Qed.
-
-Definition tailrec_fact (e d : Exp) : Exp :=
-  ELetRec [
-    (2, °ECase (˝VVar 1) [
-        ([PLit (Integer 0%Z)], ˝ttrue, ˝VVar 2);
-        ([PVar], ˝ttrue,
-          (°EApp (˝VFunId (1, 2)) 
-            [°ECall (˝erlang) (˝VLit "-"%string) [˝VVar 0; ˝VLit 1%Z];
-             °ECall (˝erlang) (˝VLit "*"%string) [˝VVar 0; ˝VVar 3]
-            ]))
-      ]
-    )
-  ] (EApp (˝VFunId (0, 2)) [e; d]) 
-.
-
-Theorem fact_tailrec_eval_ex:
-  forall (z : Z) (z' : Z), (0 <= z)%Z ->
-  exists (y : Z),
-  ⟨ [], (tailrec_fact (˝VLit z) (˝VLit z')) ⟩ -->* RValSeq [VLit y] /\ (y = Z.of_nat (Factorial.fact (Z.to_nat z)) * z')%Z.
-Proof.
-  solve_symbolically z z'.
-  
-
-Qed.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
