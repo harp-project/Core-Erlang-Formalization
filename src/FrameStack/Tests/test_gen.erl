e78ba522fbac44636402039aa458fd88cd583d9f
BenGGneB <rostabenji@gmail.com>
2026-01-25 00:57:21 +0100

    clean up and fix tests Co-authored-by: Drisi <drisi0612@gmail.com>

--- src/FrameStack/Tests/test_gen.erl
+++ src/FrameStack/Tests/test_gen.erl
@@ -1,6 +1,43 @@
 -module(test_gen).
 -export([gen/3, generate_all/0]).
 
+%% =============================================================================
+%% USAGE & EXPLANATION
+%% =============================================================================
+%%
+%% PURPOSE:
+%%   This erlang module runs the Erlang functions given in the tests section against a specific target module 
+%%   (configured below) and outputs the results in Coq format.
+%%   It generates "Goal" and "Proof" statements verifying that the Coq 
+%%   implementation behaves identically to the Erlang implementation.
+%%
+%% CONFIGURATION:
+%%   Change the ?TARGET_MODULE macro below to point to the module you want to test.
+%%     - lists     : The Erlang standard library (for baseline comparison)
+%%
+%% HOW TO RUN:
+%%   1. Compile:
+%%      $ erlc test_gen.erl target_module.erl
+%%
+%%   2. Run and print to console:
+%%      $ erl -noshell -pa . -s test_gen generate_all -s init stop
+%%
+%%   3. Run and save to file (tests.v):
+%%      $ erl -noshell -pa . -s test_gen generate_all -s init stop > tests.v
+%%
+%% ADDING TESTS:
+%%   Add new entries to the `Tests` list in generate_all/0.
+%%   Format: {FunctionName, [Arg1, Arg2, ...]}
+%%
+%% =============================================================================
+
+%% ==========================================================
+%% USER CONFIGURATION
+%% ==========================================================
+
+%% The module containing the functions we want to execute/test.
+-define(TARGET_MODULE, lists). 
+
 %% ==========================================================
 %% AST BUILDER HELPERS
 %% ==========================================================
@@ -21,21 +58,41 @@ ast_nil() -> {ast_nil}.
 %% ==========================================================
 
 generate_all() ->
-    %% AST definitions
-    FunGt0 = with_ast(fun(X) -> X > 0 end, ast_fun(1, ast_call("erlang", ">", [ast_var(0), ast_int(0)]))),
-    FunGt2 = with_ast(fun(X) -> X > 2 end, ast_fun(1, ast_call("erlang", ">", [ast_var(0), ast_int(2)]))),
-    FunEqB = with_ast(fun(X) -> X == b end, ast_fun(1, ast_call("erlang", "=:=", [ast_var(0), ast_atom(b)]))),
-    FunEven = with_ast(fun(X) -> X rem 2 == 0 end, ast_fun(1, ast_call("erlang", "=:=", [ast_call("erlang", "rem", [ast_var(0), ast_int(2)]), ast_int(0)]))),
-    FunWrapTrue = with_ast(fun(X) -> {true, X} end, ast_fun(1, ast_tuple([ast_atom(true), ast_var(0)]))),
-    FunDupList = with_ast(fun(X) -> [X, X] end, ast_fun(1, ast_cons(ast_var(0), ast_cons(ast_var(0), ast_nil())))),
-    FunPair = with_ast(fun(A, B) -> {A, B} end, ast_fun(2, ast_tuple([ast_var(0), ast_var(1)]))),
+    %% ==========================================================
+    %% AST DEFINITIONS
+    %% Structure: Name = with_ast(RealFun, AstRepresentation).
+    %% AST Note:  Variables are De Bruijn indices (ast_var(0) is the innermost argument).
+    %% Example:   fun(X) -> X > 0 end  ==  ast_fun(1, ast_call(..., [ast_var(0), ...]))
+    %% ==========================================================
+
+    FunGt0 = with_ast(fun(X) -> X > 0 end, 
+        ast_fun(1, ast_call("erlang", ">", [ast_var(0), ast_int(0)]))),
+
+    FunGt2 = with_ast(fun(X) -> X > 2 end, 
+        ast_fun(1, ast_call("erlang", ">", [ast_var(0), ast_int(2)]))),
+
+    FunEqB = with_ast(fun(X) -> X == b end, 
+        ast_fun(1, ast_call("erlang", "=:=", [ast_var(0), ast_atom(b)]))),
+
+    FunEven = with_ast(fun(X) -> X rem 2 == 0 end, 
+        ast_fun(1, ast_call("erlang", "=:=", [ast_call("erlang", "rem", [ast_var(0), ast_int(2)]), ast_int(0)]))),
+
+    FunWrapTrue = with_ast(fun(X) -> {true, X} end, 
+        ast_fun(1, ast_tuple([ast_atom(true), ast_var(0)]))),
+
+    FunDupList = with_ast(fun(X) -> [X, X] end, 
+        ast_fun(1, ast_cons(ast_var(0), ast_cons(ast_var(0), ast_nil())))),
+
+    FunPair = with_ast(fun(A, B) -> {A, B} end, 
+        ast_fun(2, ast_tuple([ast_var(0), ast_var(1)]))),
     
     Tests = [
         {all,       [FunGt0, [1, 2, 3]]},
         {any,       [FunGt2, [1, 2, 3]]},
         {member,    [b, [a, b, c]]},
         {search,    [FunEqB, [a, b, c]]},
-        {append,    [[1, 2], [3, 4]]},
+        {append,    [[1, 2], [3, 4]]},            %% append/2
+        {append,    [[[1, 2], [3, 4], [5]]]},     %% append/1
         {concat,    [[[1, 2], [3], []]]},
         {delete,    [2, [1, 2, 3, 2]]},
         {droplast,  [[1, 2, 3]]},
@@ -56,7 +113,8 @@ generate_all() ->
         {reverse,   [[1, 2, 3]]},
         {split,     [2, [1, 2, 3, 4]]},
         {splitwith, [FunGt2, [4, 3, 2, 1]]},
-        {sublist,   [[a, b, c, d], 2, 2]},
+        {sublist,   [[a, b, c, d], 2, 2]},        %% sublist/3
+        {sublist,   [[a, b, c, d], 2]},           %% sublist/2
         {subtract,  [[1, 2, 3, 4], [2, 4]]},
         {suffix,    [[3, 4], [1, 2, 3, 4]]},
         {sum,       [[1, 2, 3, 4]]},
@@ -78,8 +136,10 @@ generate_all() ->
         {merge,     [[1, 3, 5], [2, 4, 6]]}
     ],
 
+    io:format("%% Running tests against module: ~p~n", [?TARGET_MODULE]),
+
     lists:foreach(fun({Fn, Args}) -> 
-        gen(Fn, Args, std_lists) 
+        gen(Fn, Args, ?TARGET_MODULE) 
     end, Tests).
 
 
@@ -105,7 +165,7 @@ gen(Func, Args, Module) ->
             %% Using ~ts for safety on the final print
             io:format("(** Test ~p - inputs: ~p *)~n", [Func, strip_funs(RealArgs)]),
             io:format("Goal ⟨[], ~ts ~ts⟩~n", [CoqFuncName, CoqArgs]),
-            io:format("   -->* RValSeq [~ts].~n", [CoqResult]),
+            io:format("    -->* RValSeq [~ts].~n", [CoqResult]),
             io:format("Proof. auto_proove_subst_semantics. Qed.~n~n")
     catch
         _:E ->
